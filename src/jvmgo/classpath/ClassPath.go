5511937b404ee5ae50d7406f323b9043d79c62ec
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-08 19:18:46 +0800

    define ClassPathEntry

--- src/jvmgo/classpath/ClassPath.go
+++ src/jvmgo/classpath/ClassPath.go
@@ -1,5 +1,17 @@
 package classpath
 
 type ClassPath struct {
+    entries []ClassPathEntry
+}
+
+type ClassPathEntry interface {
+
+}
+
+type ClassPathDirEntry struct {
+    // todo
+}
+
+type ClassPathJarEntry struct {
     // todo
 }

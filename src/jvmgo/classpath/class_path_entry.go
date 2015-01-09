fc3ceb035c6c19bc7292e068baf850b6dd55217c
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-09 14:49:23 +0800

    add class_path_entry_jar.go

--- src/jvmgo/classpath/class_path_entry.go
+++ src/jvmgo/classpath/class_path_entry.go
@@ -9,15 +9,6 @@ type ClassPathEntry interface {
     readClassData(path string) ([]byte, error)
 }
 
-type ClassPathJarEntry struct {
-    jar string
-}
-
-func (self *ClassPathJarEntry) readClassData(path string) ([]byte, error) {
-    // todo
-    return nil, nil
-}
-
 func parseClassPathEntry(str string) (ClassPathEntry) {
     if strings.HasSuffix(str, ".jar") {
         return &ClassPathJarEntry{str}
---
+++ src/jvmgo/classpath/class_path_entry_jar.go
@@ -0,0 +1,10 @@
+package classpath
+
+type ClassPathJarEntry struct {
+    jar string
+}
+
+func (self *ClassPathJarEntry) readClassData(path string) ([]byte, error) {
+    // todo
+    return nil, nil
+}

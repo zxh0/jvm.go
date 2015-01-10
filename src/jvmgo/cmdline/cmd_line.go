2e7ff98838ef5af73fd5b8c22eb0144d895f9e93
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-10 13:57:46 +0800

    parse cmd line args

--- src/jvmgo/cmdline/cmd_line.go
+++ src/jvmgo/cmdline/cmd_line.go
@@ -1,12 +1,12 @@
 package cmdline 
 
-//import "fmt"
+import "strings"
 
 // java [ options ] class [ arguments ]
 type Command struct {
-    options []*Option
-    class   string
-    args    []string
+    options     []*Option
+    class       string
+    args        []string
 }
 
 type Option struct {
@@ -14,23 +14,51 @@ type Option struct {
     value   string
 }
 
-func (self *Command) parseOptions() {
+type CmdLineArgs struct {
+    args []string
+}
+
+func (self *CmdLineArgs) empty() (bool) {
+    return len(self.args) > 0
+}
+
+func (self *CmdLineArgs) first() (string) {
+    return self.args[0]
+}
+
+func (self *CmdLineArgs) removeFirst() (first string) {
+    first = self.args[0]
+    self.args = self.args[1:]
+    return
+}
+
+func (self *Command) parseOptions(args *CmdLineArgs) {
+    if !args.empty() {
+        if strings.HasPrefix(args.first(), "-") {
+            option := &Option{}
+            option.name = args.removeFirst()
+            if !args.empty() {
+                if true {
+                    option.value = args.removeFirst()
+                }
+            }
+        }
+    }
+}
+
+func (self *Command) parseClass(args *CmdLineArgs) {
 
 }
 
-func (self *Command) parseClass() {
+func (self *Command) parseArgs(args *CmdLineArgs) {
 
 }
 
-func (self *Command) parseArgs() {
-
-}
-
-func ParseCommand(cmdLineArgs []string) {
-    // todo
+func ParseCommand(osArgs []string) {
+    cmdLineArgs := &CmdLineArgs{osArgs[1:]}
     cmd := &Command{}
     cmd.options = []*Option{} // len == 0
-    cmd.parseOptions()
-    cmd.parseClass()
-    cmd.parseArgs()
+    cmd.parseOptions(cmdLineArgs)
+    cmd.parseClass(cmdLineArgs)
+    cmd.parseArgs(cmdLineArgs)
 }

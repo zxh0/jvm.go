3bed53dee6f5c4dde021ca33cf77a2e8fac83578
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-10 19:18:56 +0800

    delete stack.go

--- src/jvmgo/thread/stack.go
+++
@@ -1,5 +0,0 @@
-package thread
-
-type Stack struct {
-
-}
--- src/jvmgo/thread/thread.go
+++ src/jvmgo/thread/thread.go
@@ -2,4 +2,14 @@ package thread
 
 type Thread struct {
     // todo
+    pc      uint32
+    stack   *Stack
+}
+
+type Stack struct {
+    frames  []*Frame
+}
+
+type Frame struct {
+
 }

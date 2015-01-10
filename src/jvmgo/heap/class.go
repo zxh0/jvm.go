9f7fbe6ef80f8e541afa132b2dcf6d097deca461
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-10 21:29:00 +0800

    add class.go

---
+++ src/jvmgo/heap/class.go
@@ -0,0 +1,17 @@
+package heap
+
+type Class struct {
+    staticFields    []*Field
+    staticMethods   []*Method
+    fields          []*Field
+    methods         []*Method
+    // todo
+}
+
+type Field struct {
+    name    string
+}
+
+type Method struct {
+    name    string
+}

002456399cacae770cf9b9be46cf195377dc281b
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-10 15:10:48 +0800

    test parse cmd

--- src/jvmgo/cmdline/cmd_line_test.go
+++ src/jvmgo/cmdline/cmd_line_test.go
@@ -21,3 +21,33 @@ func TestParseCommandFail1(t *testing.T) {
         //t.Error(err)
     }
 }
+
+func TestParseCommandFail2(t *testing.T) {
+    args := []string{"java", "-cp", "a/b/c"}
+    _, err := ParseCommand(args)
+    if err == nil {
+        t.Error("err == nil")
+    } else {
+        //t.Error(err)
+    }
+}
+
+func TestParseCommandOK0(t *testing.T) {
+    args := []string{"java", "-cp", "a/b/c", "MyClass"}
+    _, err := ParseCommand(args)
+    if err != nil {
+        t.Error(err)
+    } else {
+        //t.Error(err)
+    }
+}
+
+func TestParseCommandOK1(t *testing.T) {
+    args := []string{"java", "-cp", "a/b/c", "MyClass", "a", "b", "c"}
+    _, err := ParseCommand(args)
+    if err != nil {
+        t.Error(err)
+    } else {
+        //t.Error(err)
+    }
+}

1283ccf47089f02bbbe885b39761e95676e06c56
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-16 19:05:25 +0800

    operand stack...

--- src/jvmgo/rtda/operand_stack.go
+++ src/jvmgo/rtda/operand_stack.go
@@ -9,27 +9,28 @@ func (self *OperandStack) push(item any) {
     self.slots[self.size] = item
     self.size++
 }
+
 func (self *OperandStack) pop() (item any) {
     item = self.slots[self.size]
     self.size--
     return
 } 
 
-func (self *OperandStack) pushInt(val int32) {
-    self.push(val)
-}
 func (self *OperandStack) popInt() (int32) {
     return self.pop().(int32)
 }
 
-func (self *OperandStack) pushFloat(val float32) {
-    self.push(val)
+func (self *OperandStack) popLong() (int64) {
+    return self.pop().(int64)
 }
+
 func (self *OperandStack) popFloat() (float32) {
     return self.pop().(float32)
-} 
+}
 
-
+func (self *OperandStack) popDouble() (float64) {
+    return self.pop().(float64)
+}
 
 func NewOperandStack(length uint16) (*OperandStack) {
     slots := make([]any, length)

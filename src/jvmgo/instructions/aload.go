ac2c7525e4d651cb25abd941550c1926249dde1b
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-17 17:55:45 +0800

    aload_0

--- src/jvmgo/instructions/aload.go
+++ src/jvmgo/instructions/aload.go
@@ -6,12 +6,20 @@ import "jvmgo/rtda"
 type aload struct {
     index uint8
 }
-
 func (self *aload) fetchOperands(bcr *BytecodeReader) {
     self.index = bcr.readUint8()
 }
-
 func (self *aload) execute(thread *rtda.Thread) {
-    ref := thread.CurrentFrame().LocalVars().GetRef(uint(self.index))
+    _aload(thread, uint(self.index))
+}
+
+type aload_0 struct {}
+func (self *aload_0) fetchOperands(bcr *BytecodeReader) {}
+func (self *aload_0) execute(thread *rtda.Thread) {
+    _aload(thread, 0)
+}
+
+func _aload(thread *rtda.Thread, index uint) {
+    ref := thread.CurrentFrame().LocalVars().GetRef(index)
     thread.CurrentFrame().OperandStack().PushRef(ref)
 }

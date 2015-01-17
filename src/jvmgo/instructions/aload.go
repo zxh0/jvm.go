1db6cce318443505923002e719a48b7b1762e10e
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-17 17:58:31 +0800

    aload_1,2,3

--- src/jvmgo/instructions/aload.go
+++ src/jvmgo/instructions/aload.go
@@ -19,6 +19,24 @@ func (self *aload_0) execute(thread *rtda.Thread) {
     _aload(thread, 0)
 }
 
+type aload_1 struct {}
+func (self *aload_1) fetchOperands(bcr *BytecodeReader) {}
+func (self *aload_1) execute(thread *rtda.Thread) {
+    _aload(thread, 1)
+}
+
+type aload_2 struct {}
+func (self *aload_2) fetchOperands(bcr *BytecodeReader) {}
+func (self *aload_2) execute(thread *rtda.Thread) {
+    _aload(thread, 2)
+}
+
+type aload_3 struct {}
+func (self *aload_3) fetchOperands(bcr *BytecodeReader) {}
+func (self *aload_3) execute(thread *rtda.Thread) {
+    _aload(thread, 3)
+}
+
 func _aload(thread *rtda.Thread, index uint) {
     ref := thread.CurrentFrame().LocalVars().GetRef(index)
     thread.CurrentFrame().OperandStack().PushRef(ref)
--- src/jvmgo/instructions/instruction.go
+++ src/jvmgo/instructions/instruction.go
@@ -18,6 +18,10 @@ func newInstruction(opcode byte) (Instruction) {
     switch opcode {
     case 0x01: return &aconst_null{}
     case 0x19: return &aload{}
+    case 0x2a: return &aload_0{}
+    case 0x2b: return &aload_1{}
+    case 0x2c: return &aload_2{}
+    case 0x2d: return &aload_3{}
     case 0x32: return &aaload{}
     case 0x53: return &aastore{}
     default: panic("BAD opcode!")

c7a3e236cb58285e02937d0a6e90ee7bbd2419fa
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-17 09:30:54 +0800

    jvm stack

--- src/jvmgo/rtda/jvm_stack.go
+++ src/jvmgo/rtda/jvm_stack.go
@@ -2,10 +2,16 @@ package rtda
 
 // jvm stack
 type Stack struct {
-    // size 
-    frames []*Frame
+    size    uint16 
+    frames  []*Frame
 }
 
-func (self *Stack) push() {
+func (self *Stack) push(frame *Frame) {
+    self.frames[self.size] = frame
+    self.size++
+}
 
+func (self *Stack) pop() {
+    self.frames[self.size] = nil
+    self.size--
 }

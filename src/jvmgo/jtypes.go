087c7841f6453a189ecb7452bf680f4b350df9d4
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-17 23:48:28 +0800

    add types.go

---
+++ src/jvmgo/jtypes.go
@@ -0,0 +1,10 @@
+package main
+
+type jboolean   uint8
+type jbyte      int8
+type jshort     int16
+type jchar      uint16
+type jint       int32
+type jlong      int64
+type jfloat     float32
+type jdouble    float64

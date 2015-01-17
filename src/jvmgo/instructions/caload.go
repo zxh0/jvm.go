9d603fec3edd81da6d01285741a01ec4bd96ece9
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-17 21:34:47 +0800

    caload

---
+++ src/jvmgo/instructions/caload.go
@@ -0,0 +1,16 @@
+package instructions
+
+import "jvmgo/rtda"
+
+// Load char from array 
+type caload struct {
+    
+}
+
+func (self *caload) fetchOperands(bcr *BytecodeReader) {
+    
+}
+
+func (self *caload) execute(thread *rtda.Thread) {
+    // todo
+}

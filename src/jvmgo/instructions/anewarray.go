e467feeed1154e5c6160b61735710c9d7b5c0c0f
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-17 18:07:08 +0800

    anewarray

---
+++ src/jvmgo/instructions/anewarray.go
@@ -0,0 +1,16 @@
+package instructions
+
+import "jvmgo/rtda"
+
+// Create new array of reference
+type anewarray struct {
+    index uint16
+}
+func (self *anewarray) fetchOperands(bcr *BytecodeReader) {
+    indexbyte1 := uint16(bcr.readUint8())
+    indexbyte2 := uint16(bcr.readUint8())
+    self.index = (indexbyte1 << 8) | indexbyte2
+}
+func (self *anewarray) execute(thread *rtda.Thread) {
+    // todo
+}

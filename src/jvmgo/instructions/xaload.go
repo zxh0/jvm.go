65e177ea50614c922965d3ca1bf33f7cb94d2ba6
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 09:20:45 +0800

    rename aaload.go to xaload.go

--- src/jvmgo/instructions/xaload.go
+++ src/jvmgo/instructions/xaload.go
@@ -3,13 +3,8 @@ package instructions
 import "jvmgo/rtda"
 
 // Load reference from array
-type aaload struct {
-    // todo
-}
-
-func (self *aaload) fetchOperands(bcr *BytecodeReader) {
-    // no operands
-}
+type aaload struct {}
+func (self *aaload) fetchOperands(bcr *BytecodeReader) {}
 
 func (self *aaload) execute(thread *rtda.Thread) {
     // todo

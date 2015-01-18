d4bffd1ceb63f165203586c16f208ce8d1601799
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 09:22:02 +0800

    delete baload.go

--- src/jvmgo/instructions/xaload.go
+++ src/jvmgo/instructions/xaload.go
@@ -5,7 +5,14 @@ import "jvmgo/rtda"
 // Load reference from array
 type aaload struct {}
 func (self *aaload) fetchOperands(bcr *BytecodeReader) {}
-
 func (self *aaload) execute(thread *rtda.Thread) {
+    // todo
+}
+
+
+// Load byte or boolean from array 
+type baload struct {}
+func (self *baload) fetchOperands(bcr *BytecodeReader) {}
+func (self *baload) execute(thread *rtda.Thread) {
     // todo
 }

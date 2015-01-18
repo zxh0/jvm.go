91fb7443829bd43bd9fe81c2224b880c13d93be9
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 09:04:52 +0800

    d2i

--- src/jvmgo/instructions/d2x.go
+++ src/jvmgo/instructions/d2x.go
@@ -11,3 +11,13 @@ func (self *d2f) execute(thread *rtda.Thread) {
     f := float32(d)
     stack.PushFloat(f)
 }
+
+// Convert double to int
+type d2i struct {}
+func (self *d2i) fetchOperands(bcr *BytecodeReader) {}
+func (self *d2i) execute(thread *rtda.Thread) {
+    stack := thread.CurrentFrame().OperandStack()
+    d := stack.PopDouble()
+    i := int32(d)
+    stack.PushInt(i)
+}

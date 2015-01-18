299f12d23c82e78be830885f3a1fd00d95a70c93
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 09:27:11 +0800

    rename astore.go to astorex.go

--- src/jvmgo/instructions/xastore.go
+++ src/jvmgo/instructions/xastore.go
@@ -3,14 +3,8 @@ package instructions
 import "jvmgo/rtda"
 
 // Store into reference array 
-type aastore struct {
-
-}
-
-func (self *aastore) fetchOperands(bcr *BytecodeReader) {
-    // no operands
-}
-
+type aastore struct {}
+func (self *aastore) fetchOperands(bcr *BytecodeReader) {}
 func (self *aastore) execute(thread *rtda.Thread) {
     // todo
 }

c735a9e1d6f8088a42170d28a6eea9a03cc09850
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 09:28:05 +0800

    delete bastore.go

--- src/jvmgo/instructions/xastore.go
+++ src/jvmgo/instructions/xastore.go
@@ -8,3 +8,10 @@ func (self *aastore) fetchOperands(bcr *BytecodeReader) {}
 func (self *aastore) execute(thread *rtda.Thread) {
     // todo
 }
+
+// Store into byte or boolean array 
+type bastore struct {}
+func (self *bastore) fetchOperands(bcr *BytecodeReader) {}
+func (self *bastore) execute(thread *rtda.Thread) {
+    // todo
+}

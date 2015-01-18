2dcf6a9b4a36c6c9b09914131d3d9f0b615d82a7
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 09:23:08 +0800

    delete caload.go

--- src/jvmgo/instructions/xaload.go
+++ src/jvmgo/instructions/xaload.go
@@ -9,10 +9,16 @@ func (self *aaload) execute(thread *rtda.Thread) {
     // todo
 }
 
-
 // Load byte or boolean from array 
 type baload struct {}
 func (self *baload) fetchOperands(bcr *BytecodeReader) {}
 func (self *baload) execute(thread *rtda.Thread) {
+    // todo
+}
+
+// Load char from array 
+type caload struct {}
+func (self *caload) fetchOperands(bcr *BytecodeReader) {}
+func (self *caload) execute(thread *rtda.Thread) {
     // todo
 }

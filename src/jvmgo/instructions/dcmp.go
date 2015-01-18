100980860676158e85e690666790db023012a910
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 18:54:12 +0800

    dcmpg,dcmpl

--- src/jvmgo/instructions/dcmp.go
+++ src/jvmgo/instructions/dcmp.go
@@ -6,11 +6,28 @@ import "jvmgo/rtda"
 type dcmpg struct {}
 func (self *dcmpg) fetchOperands(bcr *BytecodeReader) {}
 func (self *dcmpg) execute(thread *rtda.Thread) {
-    // todo
+    dcmp(thread, true)
 }
 
 type dcmpl struct {}
 func (self *dcmpl) fetchOperands(bcr *BytecodeReader) {}
 func (self *dcmpl) execute(thread *rtda.Thread) {
-    // todo
+    dcmp(thread, false)
+}
+
+func dcmp(thread *rtda.Thread, gFlag bool) {
+    stack := thread.CurrentFrame().OperandStack()
+    v1 := stack.PopDouble()
+    v2 := stack.PopDouble()
+    if v1 > v2 {
+        stack.PushInt(1)
+    } else if v1 == v2 {
+        stack.PushInt(0)
+    } else if v1 < v2 {
+        stack.PushInt(-1)
+    } else if gFlag {
+        stack.PushInt(1)
+    } else {
+        stack.PushInt(-1)
+    }
 }

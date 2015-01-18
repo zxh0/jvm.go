b6ddb19eb3e19244ea4ef56f7436ea04b40a55eb
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 20:12:45 +0800

    rename aretorn.go to xreturn.go

--- src/jvmgo/instructions/xreturn.go
+++ src/jvmgo/instructions/xreturn.go
@@ -3,14 +3,8 @@ package instructions
 import "jvmgo/rtda"
 
 // Return reference from method 
-type areturn struct {
-    
-}
-
-func (self *areturn) fetchOperands(bcr *BytecodeReader) {
-    
-}
-
+type areturn struct {}
+func (self *areturn) fetchOperands(bcr *BytecodeReader) {}
 func (self *areturn) execute(thread *rtda.Thread) {
     // todo
 }

049adc8fbcfe61231b51620636323d5ebb9e1008
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-18 19:05:31 +0800

    fadd

--- src/jvmgo/instructions/xadd.go
+++ src/jvmgo/instructions/xadd.go
@@ -12,3 +12,14 @@ func (self *dadd) execute(thread *rtda.Thread) {
     result := v1 + v2
     stack.PushDouble(result)
 }
+
+// Add float
+type fadd struct {}
+func (self *fadd) fetchOperands(bcr *BytecodeReader) {}
+func (self *fadd) execute(thread *rtda.Thread) {
+    stack := thread.CurrentFrame().OperandStack()
+    v1 := stack.PopFloat()
+    v2 := stack.PopFloat()
+    result := v1 + v2
+    stack.PushFloat(result)
+}

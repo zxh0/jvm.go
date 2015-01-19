1af1c58b4ce8267a9bbc5d6b2758e93dff35c2bb
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 16:51:23 +0800

    ishr

--- src/jvmgo/instructions/xsh.go
+++ src/jvmgo/instructions/xsh.go
@@ -12,12 +12,12 @@ func (self *ishl) execute(thread *rtda.Thread) {
     stack.PushInt(result)
 }
 
-// // Boolean OR long
-// type lor struct {NoOperandsInstruction}
-// func (self *lor) execute(thread *rtda.Thread) {
-//     stack := thread.CurrentFrame().OperandStack()
-//     v1 := stack.PopLong()
-//     v2 := stack.PopLong()
-//     result := v1 | v2
-//     stack.PushLong(result)
-// }
+// Arithmetic shift right int
+type ishr struct {NoOperandsInstruction}
+func (self *ishr) execute(thread *rtda.Thread) {
+    stack := thread.CurrentFrame().OperandStack()
+    v1 := stack.PopInt()
+    v2 := uint32(stack.PopInt()) & 0x1f
+    result := v1 >> v2 // todo
+    stack.PushInt(result)
+}

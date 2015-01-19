4770e7fd6497036e26eb1e53c19b6de369d3bd36
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 17:10:52 +0800

    code refactor

--- src/jvmgo/instructions/if_icmp.go
+++ src/jvmgo/instructions/if_icmp.go
@@ -5,47 +5,47 @@ import "jvmgo/rtda"
 // Branch if int comparison succeeds 
 type if_icmpeq struct {BranchInstruction}
 func (self *if_icmpeq) execute(thread *rtda.Thread) {
-    if val1, val2 := popInts(thread); val1 == val2 {
+    if val1, val2 := pop2Ints(thread); val1 == val2 {
         // todo
     }
 }
 
 type if_icmpne struct {BranchInstruction}
 func (self *if_icmpne) execute(thread *rtda.Thread) {
-    if val1, val2 := popInts(thread); val1 != val2 {
+    if val1, val2 := pop2Ints(thread); val1 != val2 {
         // todo
     }
 }
 
 type if_icmplt struct {BranchInstruction}
 func (self *if_icmplt) execute(thread *rtda.Thread) {
-    if val1, val2 := popInts(thread); val1 < val2 {
+    if val1, val2 := pop2Ints(thread); val1 < val2 {
         // todo
     }
 }
 
 type if_icmple struct {BranchInstruction}
 func (self *if_icmple) execute(thread *rtda.Thread) {
-    if val1, val2 := popInts(thread); val1 <= val2 {
+    if val1, val2 := pop2Ints(thread); val1 <= val2 {
         // todo
     }
 }
 
 type if_icmpgt struct {BranchInstruction}
 func (self *if_icmpgt) execute(thread *rtda.Thread) {
-    if val1, val2 := popInts(thread); val1 > val2 {
+    if val1, val2 := pop2Ints(thread); val1 > val2 {
         // todo
     }
 }
 
 type if_icmpge struct {BranchInstruction}
 func (self *if_icmpge) execute(thread *rtda.Thread) {
-    if val1, val2 := popInts(thread); val1 >= val2 {
+    if val1, val2 := pop2Ints(thread); val1 >= val2 {
         // todo
     }
 }
 
-func popInts(thread *rtda.Thread) (val1, val2 int32) {
+func pop2Ints(thread *rtda.Thread) (val1, val2 int32) {
     stack := thread.CurrentFrame().OperandStack()
     val1 = stack.PopInt()
     val2 = stack.PopInt()

da7523df58a0095586b8a23da783d84cc35f6746
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 22:49:25 +0800

    delete files

--- src/jvmgo/classfile/constants.go
+++ src/jvmgo/classfile/constants.go
@@ -122,3 +122,83 @@ func (self *ConstantNameAndTypeInfo) readInfo(reader *ClassReader) {
     self.nameIndex = reader.readUint16()
     self.descriptorIndex = reader.readUint16()
 }
+
+/*
+CONSTANT_Fieldref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+*/
+type ConstantFieldrefInfo struct {
+    classIndex       uint16
+    nameAndTypeIndex uint16
+}
+func (self *ConstantFieldrefInfo) readInfo(reader *ClassReader) {
+    self.classIndex = reader.readUint16()
+    self.nameAndTypeIndex = reader.readUint16()
+}
+
+/*
+CONSTANT_Methodref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+*/
+type ConstantMethodrefInfo struct {
+    classIndex       uint16
+    nameAndTypeIndex uint16
+}
+func (self *ConstantMethodrefInfo) readInfo(reader *ClassReader) {
+    self.classIndex = reader.readUint16()
+    self.nameAndTypeIndex = reader.readUint16()
+}
+
+/*
+CONSTANT_InterfaceMethodref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+*/
+type ConstantInterfaceMethodrefInfo struct {
+    classIndex       uint16
+    nameAndTypeIndex uint16
+}
+func (self *ConstantInterfaceMethodrefInfo) readInfo(reader *ClassReader) {
+    self.classIndex = reader.readUint16()
+    self.nameAndTypeIndex = reader.readUint16()
+}
+
+/*
+CONSTANT_MethodHandle_info {
+    u1 tag;
+    u1 reference_kind;
+    u2 reference_index;
+}
+*/
+type ConstantMethodHandleInfo struct {
+    referenceKind  uint8
+    referenceIndex uint16
+}
+func (self *ConstantMethodHandleInfo) readInfo(reader *ClassReader) {
+    self.referenceKind = reader.readUint8()
+    self.referenceIndex = reader.readUint16()
+}
+
+/*
+CONSTANT_InvokeDynamic_info {
+    u1 tag;
+    u2 bootstrap_method_attr_index;
+    u2 name_and_type_index;
+}
+*/
+type ConstantInvokeDynamicInfo struct {
+    bootstrapMethodAttrIndex uint16
+    nameAndTypeIndex         uint16
+}
+func (self *ConstantInvokeDynamicInfo) readInfo(reader *ClassReader) {
+    self.bootstrapMethodAttrIndex = reader.readUint16()
+    self.nameAndTypeIndex = reader.readUint16()
+}

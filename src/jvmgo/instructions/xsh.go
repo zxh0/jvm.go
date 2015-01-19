e7a03643950a521b1e02f80190cc4c4181892142
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 17:04:02 +0800

    code refactor

--- src/jvmgo/instructions/xsh.go
+++ src/jvmgo/instructions/xsh.go
@@ -7,8 +7,9 @@ type ishl struct {NoOperandsInstruction}
 func (self *ishl) execute(thread *rtda.Thread) {
     stack := thread.CurrentFrame().OperandStack()
     v1 := stack.PopInt()
-    v2 := uint32(stack.PopInt()) & 0x1f
-    result := v1 << v2
+    v2 := stack.PopInt()
+    s := uint32(v2) & 0x1f
+    result := v1 << s
     stack.PushInt(result)
 }
 
@@ -17,17 +18,19 @@ type ishr struct {NoOperandsInstruction}
 func (self *ishr) execute(thread *rtda.Thread) {
     stack := thread.CurrentFrame().OperandStack()
     v1 := stack.PopInt()
-    v2 := uint32(stack.PopInt()) & 0x1f
-    result := v1 >> v2 // todo
+    v2 := stack.PopInt()
+    s := uint32(v2) & 0x1f
+    result := v1 >> s // todo
     stack.PushInt(result)
 }
 
-// Arithmetic shift right int
+// Logical shift right int
 type iushr struct {NoOperandsInstruction}
 func (self *iushr) execute(thread *rtda.Thread) {
     stack := thread.CurrentFrame().OperandStack()
     v1 := stack.PopInt()
-    v2 := uint32(stack.PopInt()) & 0x1f
-    result := v1 >> v2
+    v2 := stack.PopInt()
+    s := uint32(v2) & 0x1f
+    result := v1 >> s
     stack.PushInt(result)
 }

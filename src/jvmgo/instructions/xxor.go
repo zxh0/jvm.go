0d915a884379e6d2b08384404f2e23b4e2ca4345
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 17:36:53 +0800

    ixor, lxor

---
+++ src/jvmgo/instructions/xxor.go
@@ -0,0 +1,23 @@
+package instructions
+
+import "jvmgo/rtda"
+
+// Boolean XOR int
+type ixor struct {NoOperandsInstruction}
+func (self *ixor) execute(thread *rtda.Thread) {
+    stack := thread.CurrentFrame().OperandStack()
+    v1 := stack.PopInt()
+    v2 := stack.PopInt()
+    result := v1 ^ v2
+    stack.PushInt(result)
+}
+
+// Boolean XOR long
+type lxor struct {NoOperandsInstruction}
+func (self *lxor) execute(thread *rtda.Thread) {
+    stack := thread.CurrentFrame().OperandStack()
+    v1 := stack.PopLong()
+    v2 := stack.PopLong()
+    result := v1 ^ v2
+    stack.PushLong(result)
+}

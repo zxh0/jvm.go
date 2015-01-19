9d84629958a4e5ca6ec5f56d4f8a4383373d07ac
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 22:40:06 +0800

    delete const_integer.go

--- src/jvmgo/classfile/constants.go
+++ src/jvmgo/classfile/constants.go
@@ -1,6 +1,19 @@
 package classfile
 
 /*
+CONSTANT_Integer_info {
+    u1 tag;
+    u4 bytes;
+}
+*/
+type ConstantIntegerInfo struct {
+    val int32
+}
+func (self * ConstantIntegerInfo) readInfo(reader *ClassReader) {
+    self.val = reader.readInt32()
+}
+
+/*
 CONSTANT_Class_info {
     u1 tag;
     u2 name_index;

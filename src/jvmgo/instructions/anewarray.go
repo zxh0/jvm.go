29585ac79ac050943f08e9fc7079f2e1c63853ba
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 20:55:01 +0800

    delete anewarray.go

--- src/jvmgo/instructions/anewarray.go
+++
@@ -1,9 +0,0 @@
-package instructions
-
-import "jvmgo/rtda"
-
-// Create new array of reference
-type anewarray struct {Index16Instruction}
-func (self *anewarray) execute(thread *rtda.Thread) {
-    // todo
-}
--- src/jvmgo/instructions/new.go
+++ src/jvmgo/instructions/new.go
@@ -7,3 +7,9 @@ type _new struct {Index16Instruction}
 func (self *_new) execute(thread *rtda.Thread) {
     // todo
 }
+
+// Create new array of reference
+type anewarray struct {Index16Instruction}
+func (self *anewarray) execute(thread *rtda.Thread) {
+    // todo
+}

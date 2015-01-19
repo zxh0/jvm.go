231db17190ea44b4eea57000610ba324b7857557
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 15:51:59 +0800

    iinc

---
+++ src/jvmgo/instructions/iinc.go
@@ -0,0 +1,22 @@
+package instructions
+
+import "jvmgo/rtda"
+
+// Increment local variable by constant
+type iinc struct {
+    index   uint8
+    _const  int8
+}
+
+func (self *iinc) fetchOperands(bcr *BytecodeReader) {
+    self.index = bcr.readUint8()
+    self._const = bcr.readInt8()
+}
+
+func (self *iinc) execute(thread *rtda.Thread) {
+    localVars := thread.CurrentFrame().LocalVars()
+    index := uint(self.index)
+    val := localVars.GetInt(index)
+    val += int32(self._const)
+    localVars.SetInt(index, val)
+}

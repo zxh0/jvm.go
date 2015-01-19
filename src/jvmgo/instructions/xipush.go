9d8024caa7bcb4872857b22a54cb891e329ad340
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 14:17:15 +0800

    rename bipush.go to xipush.go

--- src/jvmgo/instructions/xipush.go
+++ src/jvmgo/instructions/xipush.go
@@ -6,11 +6,9 @@ import "jvmgo/rtda"
 type bipush struct {
     _byte int8
 }
-
 func (self *bipush) fetchOperands(bcr *BytecodeReader) {
     self._byte = bcr.readInt8()
 }
-
 func (self *bipush) execute(thread *rtda.Thread) {
     i := int32(self._byte)
     thread.CurrentFrame().OperandStack().PushInt(i)

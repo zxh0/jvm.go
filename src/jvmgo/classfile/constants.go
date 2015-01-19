d986c6bc42308912de78650ca51f3ae0484043cb
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 22:44:34 +0800

    delete const_string.go

--- src/jvmgo/classfile/constants.go
+++ src/jvmgo/classfile/constants.go
@@ -69,6 +69,19 @@ func (self *ConstantUtf8Info) readInfo(reader *ClassReader) {
 }
 
 /*
+CONSTANT_String_info {
+    u1 tag;
+    u2 string_index;
+}
+*/
+type ConstantStringInfo struct {
+    stringIndex uint16
+}
+func (self *ConstantStringInfo) readInfo(reader *ClassReader) {
+    self.stringIndex = reader.readUint16()
+}
+
+/*
 CONSTANT_Class_info {
     u1 tag;
     u2 name_index;

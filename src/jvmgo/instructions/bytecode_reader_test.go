d1d7756ad439f9d733639818280fc1c783b56f71
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 10:07:23 +0800

    TestReadUint8

--- src/jvmgo/instructions/bytecode_reader_test.go
+++ src/jvmgo/instructions/bytecode_reader_test.go
@@ -12,3 +12,14 @@ func TestReadInt8(t *testing.T) {
         t.Errorf("%v", b2)
     }
 }
+
+func TestReadUint8(t *testing.T) {
+    bytecodes := []byte{0xf1, 0x17}
+    bcr := newBytecodeReader(bytecodes)
+    if b1 := bcr.readUint8(); b1 != 241 {
+        t.Errorf("%v", b1)
+    }
+    if b2 := bcr.readUint8(); b2 != 23 {
+        t.Errorf("%v", b2)
+    }
+}

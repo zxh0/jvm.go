62003341e7fdf9fc9f243762531882406d923199
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 19:55:32 +0800

    monitorenter

---
+++ src/jvmgo/instructions/monitor.go
@@ -0,0 +1,9 @@
+package instructions
+
+import "jvmgo/rtda"
+
+// Get length of array
+type monitorenter struct {NoOperandsInstruction}
+func (self *monitorenter) execute(thread *rtda.Thread) {
+    // todo
+}

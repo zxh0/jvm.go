30c196ceec7522b6677ef3e91f7872ade2573346
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 12:21:49 +0800

    land

--- src/jvmgo/instructions/xand.go
+++ src/jvmgo/instructions/xand.go
@@ -12,3 +12,14 @@ func (self *iand) execute(thread *rtda.Thread) {
     result := v1 & v2
     stack.PushInt(result)
 }
+
+// Boolean AND long
+type land struct {}
+func (self *land) fetchOperands(bcr *BytecodeReader) {}
+func (self *land) execute(thread *rtda.Thread) {
+    stack := thread.CurrentFrame().OperandStack()
+    v1 := stack.PopLong()
+    v2 := stack.PopLong()
+    result := v1 & v2
+    stack.PushLong(result)
+}

edc6804981411a46c603f31583523202365552ab
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 22:47:10 +0800

    delete file

--- src/jvmgo/classfile/constants.go
+++ src/jvmgo/classfile/constants.go
@@ -106,3 +106,19 @@ type ConstantMethodTypeInfo struct {
 func (self *ConstantMethodTypeInfo) readInfo(reader *ClassReader) {
     self.descriptorIndex = reader.readUint16()
 }
+
+/*
+CONSTANT_NameAndType_info {
+    u1 tag;
+    u2 name_index;
+    u2 descriptor_index;
+}
+*/
+type ConstantNameAndTypeInfo struct {
+    nameIndex       uint16
+    descriptorIndex uint16
+}
+func (self *ConstantNameAndTypeInfo) readInfo(reader *ClassReader) {
+    self.nameIndex = reader.readUint16()
+    self.descriptorIndex = reader.readUint16()
+}

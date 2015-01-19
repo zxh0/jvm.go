d72307efe27ceac2829d1f47a44b8d7626562f4d
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-19 22:43:01 +0800

    delete const_double.go

--- src/jvmgo/classfile/constants.go
+++ src/jvmgo/classfile/constants.go
@@ -41,6 +41,20 @@ func (self *ConstantLongInfo) readInfo(reader *ClassReader) {
 }
 
 /*
+CONSTANT_Double_info {
+    u1 tag;
+    u4 high_bytes;
+    u4 low_bytes;
+}
+*/
+type ConstantDoubleInfo struct {
+    val float64
+}
+func (self *ConstantDoubleInfo) readInfo(reader *ClassReader) {
+    self.val = reader.readFloat64()
+}
+
+/*
 CONSTANT_Class_info {
     u1 tag;
     u2 name_index;

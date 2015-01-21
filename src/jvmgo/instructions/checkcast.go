1f67e2032eee6d6f900c6e55080edd9389eda24f
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-21 17:25:54 +0800

    instanceof...

--- src/jvmgo/instructions/checkcast.go
+++ src/jvmgo/instructions/checkcast.go
@@ -1,9 +1,19 @@
 package instructions
 
-import "jvmgo/rtda"
+import (
+    "jvmgo/rtda"
+    // rtc "jvmgo/rtda/class"
+)
 
 // Check whether object is of given type
 type checkcast struct {Index16Instruction}
 func (self *checkcast) Execute(thread *rtda.Thread) {
+    // frame := thread.CurrentFrame()
+    // stack := frame.OperandStack()
+    // ref := stack.PopRef()
+
+    // cp := frame.Method().Class().ConstantPool()
+    // cClass := cp.GetConstant(self.index).(rtc.ConstantClass)
+    // class := cClass.Class()
     // todo
 }
--- src/jvmgo/instructions/instanceof.go
+++ src/jvmgo/instructions/instanceof.go
@@ -1,9 +1,28 @@
 package instructions
 
-import "jvmgo/rtda"
+import (
+    "jvmgo/rtda"
+    rtc "jvmgo/rtda/class"
+)
 
 // Determine if object is of given type
 type instanceof struct {Index16Instruction}
 func (self *instanceof) Execute(thread *rtda.Thread) {
+    frame := thread.CurrentFrame()
+    stack := frame.OperandStack()
+    ref := stack.PopRef()
+
+    cp := frame.Method().Class().ConstantPool()
+    cClass := cp.GetConstant(self.index).(rtc.ConstantClass)
+    class := cClass.Class()
+
     // todo
+    if _instanceof(ref, class) {
+        
+    }
+}
+
+func _instanceof(ref *rtc.Obj, class *rtc.Class) (bool) {
+    // todo
+    return false
 }

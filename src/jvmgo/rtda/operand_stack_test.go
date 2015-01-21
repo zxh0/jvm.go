37a8fb95b03d31a94919bc1dabb41a1f364c5845
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-21 10:52:45 +0800

    test operand stack...

--- src/jvmgo/rtda/operand_stack_test.go
+++ src/jvmgo/rtda/operand_stack_test.go
@@ -9,7 +9,23 @@ func TestPushAndPop(t *testing.T) {
     stack := newOperandStack(8)
     stack.PushNull()
     stack.PushRef(class.NewArray(4, 4))
+    stack.PushInt(-37)
+    stack.PushLong(0xabcd1234ff)
+    stack.PushFloat(3.14)
+    stack.PushDouble(-2.71828)
 
+    if x:= stack.PopDouble(); x != -2.71828 {
+        t.Errorf("double:%v", x)
+    }
+    if x:= stack.PopFloat(); x != 3.14 {
+        t.Errorf("float:%v", x)
+    }
+    if x := stack.PopLong(); x != 0xabcd1234ff {
+        t.Errorf("long:%v", x)
+    }
+    if x := stack.PopInt(); x != -37 {
+        t.Errorf("int:%v", x)
+    }
     if x := stack.PopRef(); x == nil {
         t.Errorf("nil!")
     }

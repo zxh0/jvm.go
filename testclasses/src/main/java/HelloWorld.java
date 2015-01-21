dfe1674167cf0ccebe66b0dadd13e3b3f8a183b2
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-21 18:08:30 +0800

    change testclasses to a gradle project

--- testclasses/src/main/java/HelloWorld.java
+++ testclasses/src/main/java/HelloWorld.java
@@ -1,7 +1,10 @@
+/**
+ * 
+ */
 public class HelloWorld {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, world!");
     }
-
+    
 }

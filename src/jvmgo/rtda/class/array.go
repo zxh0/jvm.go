316283950e20583c595af24e3e3c8558c40d9665
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-21 17:13:11 +0800

    arraylength..

--- src/jvmgo/rtda/class/array.go
+++ src/jvmgo/rtda/class/array.go
@@ -14,11 +14,11 @@ const (
 
 func NewPrimitiveArray(atype uint8, count int32) (*Obj) {
     switch atype {
-    case AT_BOOLEAN: return &Obj{make([]uint8, count)}
+    case AT_BOOLEAN: return &Obj{make([]int8, count)}
     case AT_CHAR:    return &Obj{make([]uint16, count)}
     case AT_FLOAT:   return &Obj{make([]float32, count)}
     case AT_DOUBLE:  return &Obj{make([]float64, count)}
-    case AT_BYTE:    return &Obj{make([]uint8, count)}
+    case AT_BYTE:    return &Obj{make([]int8, count)}
     case AT_SHORT:   return &Obj{make([]int16, count)}
     case AT_INT:     return &Obj{make([]int32, count)}
     case AT_LONG:    return &Obj{make([]int64, count)}
@@ -32,6 +32,15 @@ func NewRefArray(count int32) (*Obj) {
 }
 
 func ArrayLength(arr *Obj) (int32) {
-    // todo
-    return 0
+    switch arr.fields.(type) {
+        case []int8: return int32(len(arr.fields.([]int8)))
+        case []int16: return int32(len(arr.fields.([]int16)))
+        case []int32: return int32(len(arr.fields.([]int32)))
+        case []int64: return int32(len(arr.fields.([]int64)))
+        case []uint16: return int32(len(arr.fields.([]uint16)))
+        case []float32: return int32(len(arr.fields.([]float32)))
+        case []float64: return int32(len(arr.fields.([]float64)))
+        case []*Obj: return int32(len(arr.fields.([]*Obj)))
+        default: panic("Not array!") // todo
+    }
 }

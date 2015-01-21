3ae5ea611d8486e6ff9739df1fad5dad89ef6071
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-21 16:22:18 +0800

    dreturn,freturn,ireturn,lreturn

--- src/jvmgo/instructions/xreturn.go
+++ src/jvmgo/instructions/xreturn.go
@@ -20,23 +20,35 @@ func (self *areturn) Execute(thread *rtda.Thread) {
 // Return double from method 
 type dreturn struct {NoOperandsInstruction}
 func (self *dreturn) Execute(thread *rtda.Thread) {
-    // todo
+    currentFrame := thread.PopFrame()
+    invokerFrame := thread.TopFrame()
+    val := currentFrame.OperandStack().PopDouble()
+    invokerFrame.OperandStack().PushDouble(val)
 }
 
 // Return float from method 
 type freturn struct {NoOperandsInstruction}
 func (self *freturn) Execute(thread *rtda.Thread) {
-    // todo
+    currentFrame := thread.PopFrame()
+    invokerFrame := thread.TopFrame()
+    val := currentFrame.OperandStack().PopFloat()
+    invokerFrame.OperandStack().PushFloat(val)
 }
 
 // Return int from method 
 type ireturn struct {NoOperandsInstruction}
 func (self *ireturn) Execute(thread *rtda.Thread) {
-    // todo
+    currentFrame := thread.PopFrame()
+    invokerFrame := thread.TopFrame()
+    val := currentFrame.OperandStack().PopInt()
+    invokerFrame.OperandStack().PushInt(val)
 }
 
 // Return double from method 
 type lreturn struct {NoOperandsInstruction}
 func (self *lreturn) Execute(thread *rtda.Thread) {
-    // todo
+    currentFrame := thread.PopFrame()
+    invokerFrame := thread.TopFrame()
+    val := currentFrame.OperandStack().PopLong()
+    invokerFrame.OperandStack().PushLong(val)
 }

c463801d423d8aa54e11e5b47f83beffbba534a5
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-22 20:28:14 +0800

    code refactor

--- src/jvmgo/classfile/member_info.go
+++ src/jvmgo/classfile/member_info.go
@@ -8,15 +8,6 @@ field_info {
     u2             attributes_count;
     attribute_info attributes[attributes_count];
 }
-*/
-type FieldInfo struct {
-    accessFlags     uint16
-    nameIndex       uint16
-    descriptorIndex uint16
-    attributes      []AttributeInfo
-}
-
-/*
 method_info {
     u2             access_flags;
     u2             name_index;
@@ -25,11 +16,22 @@ method_info {
     attribute_info attributes[attributes_count];
 }
 */
-type MethodInfo struct {
-    FieldInfo
+type MemberInfo struct {
+    accessFlags     uint16
+    nameIndex       uint16
+    descriptorIndex uint16
+    attributes      []AttributeInfo
 }
 
-func (self *FieldInfo) read(reader *ClassReader, cp *ConstantPool) {
+type FieldInfo struct {
+    MemberInfo
+}
+
+type MethodInfo struct {
+    MemberInfo
+}
+
+func (self *MemberInfo) read(reader *ClassReader, cp *ConstantPool) {
     self.accessFlags = reader.readUint16()
     self.nameIndex = reader.readUint16()
     self.descriptorIndex = reader.readUint16()

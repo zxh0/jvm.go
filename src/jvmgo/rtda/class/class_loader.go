5c621dd73d0d15b709d95eef1813bd5abb4bdf6a
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-22 17:25:33 +0800

    code refactor

--- src/jvmgo/rtda/class/class_loader.go
+++ src/jvmgo/rtda/class/class_loader.go
@@ -11,12 +11,28 @@ type ClassLoader struct {
     classMap    map[string]*Class
 }
 
-func (self ClassLoader) LoadClass(name string) (*Class) {
+func (self *ClassLoader) LoadClass(name string) (*Class) {
     class := self.classMap[name]
     if class != nil {
+        // already loaded
         return class
     }
 
+    self.reallyLoadClass(name)
+
+    // todo load super class
+    if class.superClassName != "" {
+        self.LoadClass(class.superClassName)
+    }
+    // todo load interfaces
+    for _, interfaceName := range class.interfaceNames {
+        self.LoadClass(interfaceName)
+    }
+
+    return class
+}
+
+func (self *ClassLoader) reallyLoadClass(name string) {
     log.Printf("load class: %v", name)
     classData, err := self.classPath.ReadClassData(name)
     if err != nil {
@@ -30,17 +46,7 @@ func (self ClassLoader) LoadClass(name string) (*Class) {
         panic("failed to parse class file:" + name + "!" + err.Error())
     }
 
-    class = newClass(cf)
-    self.classMap[name] = class
-
-    // todo load super class
-    if class.superClassName != "" {
-        self.LoadClass(class.superClassName)
-    }
-    // todo load interfaces
-    
-
-    return class
+    self.classMap[name] = newClass(cf)
 }
 
 func NewClassLoader(cp *classpath.ClassPath) (*ClassLoader) {

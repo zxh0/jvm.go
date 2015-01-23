dc57bb13c9dec268226b732b9f58e66235bba500
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-23 11:02:34 +0800

    code refactor

--- src/jvmgo/classfile/const_member.go
+++ src/jvmgo/classfile/const_member.go
@@ -6,34 +6,34 @@ CONSTANT_Fieldref_info {
     u2 class_index;
     u2 name_and_type_index;
 }
-*/
-type ConstantFieldrefInfo struct {
-    classIndex       uint16
-    nameAndTypeIndex uint16
-}
-func (self *ConstantFieldrefInfo) readInfo(reader *ClassReader) {
-    self.classIndex = reader.readUint16()
-    self.nameAndTypeIndex = reader.readUint16()
-}
-
-/*
 CONSTANT_Methodref_info {
     u1 tag;
     u2 class_index;
     u2 name_and_type_index;
 }
-*/
-type ConstantMethodrefInfo struct {
-    ConstantFieldrefInfo
-}
-
-/*
 CONSTANT_InterfaceMethodref_info {
     u1 tag;
     u2 class_index;
     u2 name_and_type_index;
 }
 */
+type ConstantMemberrefInfo struct {
+    classIndex       uint16
+    nameAndTypeIndex uint16
+}
+func (self *ConstantMemberrefInfo) readInfo(reader *ClassReader) {
+    self.classIndex = reader.readUint16()
+    self.nameAndTypeIndex = reader.readUint16()
+}
+
+type ConstantFieldrefInfo struct {
+    ConstantMemberrefInfo
+}
+
+type ConstantMethodrefInfo struct {
+    ConstantMemberrefInfo
+}
+
 type ConstantInterfaceMethodrefInfo struct {
-    ConstantFieldrefInfo
+    ConstantMemberrefInfo
 }

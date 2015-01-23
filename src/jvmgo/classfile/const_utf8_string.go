c4866ded20dda111b80794dfecd9df27a4c772d4
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-23 11:04:49 +0800

    create const_utf8_string.go

---
+++ src/jvmgo/classfile/const_utf8_string.go
@@ -0,0 +1,28 @@
+package classfile
+
+/*
+CONSTANT_Utf8_info {
+    u1 tag;
+    u2 length;
+    u1 bytes[length];
+}
+*/
+type ConstantUtf8Info struct {
+    str string
+}
+func (self *ConstantUtf8Info) readInfo(reader *ClassReader) {
+    self.str = reader.readString()
+}
+
+/*
+CONSTANT_String_info {
+    u1 tag;
+    u2 string_index;
+}
+*/
+type ConstantStringInfo struct {
+    stringIndex uint16
+}
+func (self *ConstantStringInfo) readInfo(reader *ClassReader) {
+    self.stringIndex = reader.readUint16()
+}
--- src/jvmgo/classfile/constants.go
+++ src/jvmgo/classfile/constants.go
@@ -1,33 +1,6 @@
 package classfile
 
 /*
-CONSTANT_Utf8_info {
-    u1 tag;
-    u2 length;
-    u1 bytes[length];
-}
-*/
-type ConstantUtf8Info struct {
-    str string
-}
-func (self *ConstantUtf8Info) readInfo(reader *ClassReader) {
-    self.str = reader.readString()
-}
-
-/*
-CONSTANT_String_info {
-    u1 tag;
-    u2 string_index;
-}
-*/
-type ConstantStringInfo struct {
-    stringIndex uint16
-}
-func (self *ConstantStringInfo) readInfo(reader *ClassReader) {
-    self.stringIndex = reader.readUint16()
-}
-
-/*
 CONSTANT_Class_info {
     u1 tag;
     u2 name_index;

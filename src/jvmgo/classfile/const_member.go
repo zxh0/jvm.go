97bf9892ac2369aa9590fa18549d0b4191c0a54a
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-23 11:00:36 +0800

    create const_member.go

---
+++ src/jvmgo/classfile/const_member.go
@@ -0,0 +1,39 @@
+package classfile
+
+/*
+CONSTANT_Fieldref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+*/
+type ConstantFieldrefInfo struct {
+    classIndex       uint16
+    nameAndTypeIndex uint16
+}
+func (self *ConstantFieldrefInfo) readInfo(reader *ClassReader) {
+    self.classIndex = reader.readUint16()
+    self.nameAndTypeIndex = reader.readUint16()
+}
+
+/*
+CONSTANT_Methodref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+*/
+type ConstantMethodrefInfo struct {
+    ConstantFieldrefInfo
+}
+
+/*
+CONSTANT_InterfaceMethodref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+*/
+type ConstantInterfaceMethodrefInfo struct {
+    ConstantFieldrefInfo
+}
--- src/jvmgo/classfile/constants.go
+++ src/jvmgo/classfile/constants.go
@@ -70,44 +70,6 @@ func (self *ConstantNameAndTypeInfo) readInfo(reader *ClassReader) {
 }
 
 /*
-CONSTANT_Fieldref_info {
-    u1 tag;
-    u2 class_index;
-    u2 name_and_type_index;
-}
-*/
-type ConstantFieldrefInfo struct {
-    classIndex       uint16
-    nameAndTypeIndex uint16
-}
-func (self *ConstantFieldrefInfo) readInfo(reader *ClassReader) {
-    self.classIndex = reader.readUint16()
-    self.nameAndTypeIndex = reader.readUint16()
-}
-
-/*
-CONSTANT_Methodref_info {
-    u1 tag;
-    u2 class_index;
-    u2 name_and_type_index;
-}
-*/
-type ConstantMethodrefInfo struct {
-    ConstantFieldrefInfo
-}
-
-/*
-CONSTANT_InterfaceMethodref_info {
-    u1 tag;
-    u2 class_index;
-    u2 name_and_type_index;
-}
-*/
-type ConstantInterfaceMethodrefInfo struct {
-    ConstantFieldrefInfo
-}
-
-/*
 CONSTANT_MethodHandle_info {
     u1 tag;
     u1 reference_kind;

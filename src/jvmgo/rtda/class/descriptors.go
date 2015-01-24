3434c9c5a08ff616e2242c8326b865ea5c26c135
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-24 17:00:27 +0800

    add descriptors_test.go

--- src/jvmgo/rtda/class/descriptors.go
+++ src/jvmgo/rtda/class/descriptors.go
@@ -3,26 +3,31 @@ package class
 func calcArgCount(descriptor string) (uint) {
     count := 0
     refStarted := false
-    for _, char := range descriptor {
-        if char == ';' {
-            refStarted = false
-        } else if !refStarted {
-            switch char {
-            case 'B': fallthrough // byte
-            case 'C': fallthrough // char
-            case 'D': fallthrough // double
-            case 'F': fallthrough // float
-            case 'I': fallthrough // int
-            case 'J': fallthrough // long
-            case 'S': fallthrough // short
-            case 'z': count++     // boolean
-            case 'L': fallthrough // reference
-            case '[':             // array
-                count++
-                refStarted = true
+    for pos, char := range descriptor {
+        if pos > 0 {
+            if refStarted {
+                if char == ';' {
+                    refStarted = false
+                }
+            } else {
+                switch char {
+                case ')': break
+                case 'B': fallthrough // byte
+                case 'C': fallthrough // char
+                case 'D': fallthrough // double
+                case 'F': fallthrough // float
+                case 'I': fallthrough // int
+                case 'J': fallthrough // long
+                case 'S': fallthrough // short
+                case 'z': count++     // boolean
+                case 'L': fallthrough // reference
+                case '[':             // array
+                    count++
+                    refStarted = true
+                default: panic("calcArgCount()") // todo
+                }
             }
         }
     }
-    // todo
-    return 0
+    return uint(count - 1)
 }
---
+++ src/jvmgo/rtda/class/descriptors_test.go
@@ -0,0 +1,11 @@
+package class
+
+import (
+    "testing"
+)
+
+func TestCalcArgCount(t *testing.T) {
+    if count := calcArgCount("(IDLjava/lang/Thread;)Ljava/lang/Object;"); count != 3 {
+        t.Errorf("%v", count)
+    }
+}

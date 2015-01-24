79ae38f72e07cfc6a300afae4602d09a24ae46f1
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-24 13:40:09 +0800

    system_nanoTime()

--- src/jvmgo/native/java_lang.go
+++ src/jvmgo/native/java_lang.go
@@ -1,16 +1,17 @@
 package native
 
 import (
+    "time"
     "jvmgo/rtda"
     rtc "jvmgo/rtda/class"
 )
 
-var system_nanoTime = func(stack *rtda.OperandStack) {
-    // todo
-    panic("nannannanatime...")
-}
-
 // register native methods
 func init() {
     rtc.RegisterNativeMethod("java/lang/System","nanoTime","()J", system_nanoTime)
+}
+
+func system_nanoTime(stack *rtda.OperandStack) {
+    nanoTime := time.Now().UnixNano()
+    stack.PushLong(nanoTime)
 }

c6ca25816ad0f1639a2c1f52148fe0b510bb3dc7
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-24 18:31:42 +0800

    fix calcArgCount()

--- src/jvmgo/classfile/member_descriptors.go
+++ src/jvmgo/classfile/member_descriptors.go
@@ -1,33 +1,29 @@
 package classfile
 
+// descriptor looks like: (IDLjava/lang/Thread;)Ljava/lang/Object;
 func calcArgCount(descriptor string) (uint) {
     count := 0
     refStarted := false
     for pos, char := range descriptor {
-        if pos > 0 {
-            if refStarted {
-                if char == ';' {
-                    refStarted = false
-                }
-            } else {
-                switch char {
-                case ')': break
-                case 'B': fallthrough // byte
-                case 'C': fallthrough // char
-                case 'D': fallthrough // double
-                case 'F': fallthrough // float
-                case 'I': fallthrough // int
-                case 'J': fallthrough // long
-                case 'S': fallthrough // short
-                case 'z': count++     // boolean
-                case 'L': fallthrough // reference
-                case '[':             // array
-                    count++
-                    refStarted = true
-                default: panic("calcArgCount()") // todo
-                }
+        if pos == 0 {
+            if char != '(' {
+                panic("BAD method descriptor: " + descriptor)
+            }
+        } else if refStarted {
+            if char == ';' {
+                refStarted = false
+            }
+        } else {
+            switch char {
+            case 'B', 'C', 'D', 'F', 'I', 'J', 'S', 'Z':
+                count++
+            case 'L', '[':
+                count++
+                refStarted = true
+            case ')': return uint(count) // break not works
+            default: panic("BAD method descriptor: " + descriptor)
             }
         }
     }
-    return uint(count - 1)
+    return uint(count)
 }
--- src/jvmgo/classfile/member_descriptors_test.go
+++ src/jvmgo/classfile/member_descriptors_test.go
@@ -5,6 +5,12 @@ import (
 )
 
 func TestCalcArgCount(t *testing.T) {
+    if count := calcArgCount("()V"); count != 0 {
+        t.Errorf("%v", count)
+    }
+    if count := calcArgCount("(I)F"); count != 1 {
+        t.Errorf("%v", count)
+    }
     if count := calcArgCount("(IDLjava/lang/Thread;)Ljava/lang/Object;"); count != 3 {
         t.Errorf("%v", count)
     }

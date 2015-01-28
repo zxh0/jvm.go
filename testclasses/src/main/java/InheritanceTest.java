651690900b8acdc8bfd1c4a48efb602e25fdea26
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-28 15:18:07 +0800

    InheritanceTest.java

--- testclasses/src/main/java/InheritanceTest.java
+++ testclasses/src/main/java/InheritanceTest.java
@@ -6,6 +6,15 @@ public class InheritanceTest {
         sub.y = 2L;
         sub.a = 3.14f;
         sub.b = 2.71828;
+        
+        int x = sub.x;
+        long y = sub.y;
+        float a = sub.a;
+        double b = sub.b;
+        
+        Super sup = sub;
+        long z = sup.x + sup.y;
+        
         System.out.println("OK!");
     }
     

a1bb522143b5810d6e9f3f81c1df90ab4eb8d3ee
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-28 17:45:25 +0800

    refactor FieldsTest

--- testclasses/src/main/java/FieldsTest.java
+++ testclasses/src/main/java/FieldsTest.java
@@ -1,18 +1,18 @@
 public class FieldsTest {
     
     static int x;
-    static long y;
-    float a;
-    double b;
+    static int y;
+    int a;
+    int b;
     
     public static void main(String[] args) {
         FieldsTest.x = 100;
-        FieldsTest.y = 200L;
+        FieldsTest.y = 200;
         long z = FieldsTest.x / FieldsTest.y;
         FieldsTest t = new FieldsTest();
-        t.a = 3.14f;
-        t.b = 2.71828;
-        double c = t.a / t.b;
+        t.a = 100;
+        t.b = 200;
+        int c = t.a / t.b;
         System.out.println("OK!");
     }
     

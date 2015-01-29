ee659d015922bc318f07b8fc3000d0df2925e390
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 16:03:55 +0800

    refactor xreturn

--- src/jvmgo/instructions/xreturn.go
+++ src/jvmgo/instructions/xreturn.go
@@ -4,13 +4,15 @@ import "jvmgo/rtda"
 
 // Return void from method 
 type return_ struct {NoOperandsInstruction}
-func (self *return_) Execute(thread *rtda.Thread) {
+func (self *return_) Execute(frame *rtda.Frame) {
+    thread := frame.Thread()
     thread.PopFrame()
 }
 
 // Return reference from method 
 type areturn struct {NoOperandsInstruction}
-func (self *areturn) Execute(thread *rtda.Thread) {
+func (self *areturn) Execute(frame *rtda.Frame) {
+    thread := frame.Thread()
     currentFrame := thread.PopFrame()
     invokerFrame := thread.TopFrame()
     ref := currentFrame.OperandStack().PopRef()
@@ -19,7 +21,8 @@ func (self *areturn) Execute(thread *rtda.Thread) {
 
 // Return double from method 
 type dreturn struct {NoOperandsInstruction}
-func (self *dreturn) Execute(thread *rtda.Thread) {
+func (self *dreturn) Execute(frame *rtda.Frame) {
+    thread := frame.Thread()
     currentFrame := thread.PopFrame()
     invokerFrame := thread.TopFrame()
     val := currentFrame.OperandStack().PopDouble()
@@ -28,7 +31,8 @@ func (self *dreturn) Execute(thread *rtda.Thread) {
 
 // Return float from method 
 type freturn struct {NoOperandsInstruction}
-func (self *freturn) Execute(thread *rtda.Thread) {
+func (self *freturn) Execute(frame *rtda.Frame) {
+    thread := frame.Thread()
     currentFrame := thread.PopFrame()
     invokerFrame := thread.TopFrame()
     val := currentFrame.OperandStack().PopFloat()
@@ -37,7 +41,8 @@ func (self *freturn) Execute(thread *rtda.Thread) {
 
 // Return int from method 
 type ireturn struct {NoOperandsInstruction}
-func (self *ireturn) Execute(thread *rtda.Thread) {
+func (self *ireturn) Execute(frame *rtda.Frame) {
+    thread := frame.Thread()
     currentFrame := thread.PopFrame()
     invokerFrame := thread.TopFrame()
     val := currentFrame.OperandStack().PopInt()
@@ -46,7 +51,8 @@ func (self *ireturn) Execute(thread *rtda.Thread) {
 
 // Return double from method 
 type lreturn struct {NoOperandsInstruction}
-func (self *lreturn) Execute(thread *rtda.Thread) {
+func (self *lreturn) Execute(frame *rtda.Frame) {
+    thread := frame.Thread()
     currentFrame := thread.PopFrame()
     invokerFrame := thread.TopFrame()
     val := currentFrame.OperandStack().PopLong()

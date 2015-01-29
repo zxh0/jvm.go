e51b0e68a9404d70f7daeb8eaf51a9e0a991b9f1
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:54:22 +0800

    refactor xsub

--- src/jvmgo/instructions/xsub.go
+++ src/jvmgo/instructions/xsub.go
@@ -4,8 +4,8 @@ import "jvmgo/rtda"
 
 // Subtract double
 type dsub struct {NoOperandsInstruction}
-func (self *dsub) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *dsub) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopDouble()
     v1 := stack.PopDouble()
     result := v1 - v2
@@ -14,8 +14,8 @@ func (self *dsub) Execute(thread *rtda.Thread) {
 
 // Subtract float
 type fsub struct {NoOperandsInstruction}
-func (self *fsub) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *fsub) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopFloat()
     v1 := stack.PopFloat()
     result := v1 - v2
@@ -24,8 +24,8 @@ func (self *fsub) Execute(thread *rtda.Thread) {
 
 // Subtract int
 type isub struct {NoOperandsInstruction}
-func (self *isub) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *isub) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopInt()
     v1 := stack.PopInt()
     result := v1 - v2
@@ -34,8 +34,8 @@ func (self *isub) Execute(thread *rtda.Thread) {
 
 // Subtract long
 type lsub struct {NoOperandsInstruction}
-func (self *lsub) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *lsub) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopLong()
     v1 := stack.PopLong()
     result := v1 - v2

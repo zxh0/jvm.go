830ad3b772a4002b15fe855ccdf38b19cc07f642
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:27:44 +0800

    refactor dload

--- src/jvmgo/instructions/dload.go
+++ src/jvmgo/instructions/dload.go
@@ -4,31 +4,31 @@ import "jvmgo/rtda"
 
 // Load double from local variable 
 type dload struct {Index8Instruction}
-func (self *dload) Execute(thread *rtda.Thread) {
-    _dload(thread, uint(self.index))
+func (self *dload) Execute(frame *rtda.Frame) {
+    _dload(frame, uint(self.index))
 }
 
 type dload_0 struct {NoOperandsInstruction}
-func (self *dload_0) Execute(thread *rtda.Thread) {
-    _dload(thread, 0)
+func (self *dload_0) Execute(frame *rtda.Frame) {
+    _dload(frame, 0)
 }
 
 type dload_1 struct {NoOperandsInstruction}
-func (self *dload_1) Execute(thread *rtda.Thread) {
-    _dload(thread, 1)
+func (self *dload_1) Execute(frame *rtda.Frame) {
+    _dload(frame, 1)
 }
 
 type dload_2 struct {NoOperandsInstruction}
-func (self *dload_2) Execute(thread *rtda.Thread) {
-    _dload(thread, 2)
+func (self *dload_2) Execute(frame *rtda.Frame) {
+    _dload(frame, 2)
 }
 
 type dload_3 struct {NoOperandsInstruction}
-func (self *dload_3) Execute(thread *rtda.Thread) {
-    _dload(thread, 3)
+func (self *dload_3) Execute(frame *rtda.Frame) {
+    _dload(frame, 3)
 }
 
-func _dload(thread *rtda.Thread, index uint) {
-    val := thread.CurrentFrame().LocalVars().GetDouble(index)
-    thread.CurrentFrame().OperandStack().PushDouble(val)
+func _dload(frame *rtda.Frame, index uint) {
+    val := frame.LocalVars().GetDouble(index)
+    frame.OperandStack().PushDouble(val)
 }

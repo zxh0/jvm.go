530ba57111c7848b3b21d5c4e22c8bb8dee476fe
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:51:52 +0800

    refactor xneg

--- src/jvmgo/instructions/xneg.go
+++ src/jvmgo/instructions/xneg.go
@@ -4,32 +4,32 @@ import "jvmgo/rtda"
 
 // Negate double
 type dneg struct {NoOperandsInstruction}
-func (self *dneg) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *dneg) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     val := stack.PopDouble()
     stack.PushDouble(-val)
 }
 
 // Negate float
 type fneg struct {NoOperandsInstruction}
-func (self *fneg) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *fneg) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     val := stack.PopFloat()
     stack.PushFloat(-val)
 }
 
 // Negate int
 type ineg struct {NoOperandsInstruction}
-func (self *ineg) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *ineg) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     val := stack.PopInt()
     stack.PushInt(-val)
 }
 
 // Negate long
 type lneg struct {NoOperandsInstruction}
-func (self *lneg) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *lneg) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     val := stack.PopLong()
     stack.PushLong(-val)
 }

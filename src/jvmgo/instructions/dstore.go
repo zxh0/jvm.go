df4885106d3ccfa01218124c876403c075e63731
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:28:31 +0800

    refactor dstore

--- src/jvmgo/instructions/dstore.go
+++ src/jvmgo/instructions/dstore.go
@@ -4,31 +4,31 @@ import "jvmgo/rtda"
 
 // Store double into local variable 
 type dstore struct {Index8Instruction}
-func (self *dstore) Execute(thread *rtda.Thread) {
-    _dstore(thread, uint(self.index))
+func (self *dstore) Execute(frame *rtda.Frame) {
+    _dstore(frame, uint(self.index))
 }
 
 type dstore_0 struct {NoOperandsInstruction}
-func (self *dstore_0) Execute(thread *rtda.Thread) {
-    _dstore(thread, 0)
+func (self *dstore_0) Execute(frame *rtda.Frame) {
+    _dstore(frame, 0)
 }
 
 type dstore_1 struct {NoOperandsInstruction}
-func (self *dstore_1) Execute(thread *rtda.Thread) {
-    _dstore(thread, 1)
+func (self *dstore_1) Execute(frame *rtda.Frame) {
+    _dstore(frame, 1)
 }
 
 type dstore_2 struct {NoOperandsInstruction}
-func (self *dstore_2) Execute(thread *rtda.Thread) {
-    _dstore(thread, 2)
+func (self *dstore_2) Execute(frame *rtda.Frame) {
+    _dstore(frame, 2)
 }
 
 type dstore_3 struct {NoOperandsInstruction}
-func (self *dstore_3) Execute(thread *rtda.Thread) {
-    _dstore(thread, 3)
+func (self *dstore_3) Execute(frame *rtda.Frame) {
+    _dstore(frame, 3)
 }
 
-func _dstore(thread *rtda.Thread, index uint) {
-    val := thread.CurrentFrame().OperandStack().PopDouble()
-    thread.CurrentFrame().LocalVars().SetDouble(index, val)
+func _dstore(frame *rtda.Frame, index uint) {
+    val := frame.OperandStack().PopDouble()
+    frame.LocalVars().SetDouble(index, val)
 }

aacbccd9694854dae6007c6750f3a1603a87e5b2
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:20:58 +0800

    refactor astore

--- src/jvmgo/instructions/astore.go
+++ src/jvmgo/instructions/astore.go
@@ -4,31 +4,31 @@ import "jvmgo/rtda"
 
 // Store reference into local variable 
 type astore struct {Index8Instruction}
-func (self *astore) Execute(thread *rtda.Thread) {
-    _astore(thread, uint(self.index))
+func (self *astore) Execute(frame *rtda.Frame) {
+    _astore(frame, uint(self.index))
 }
 
 type astore_0 struct {NoOperandsInstruction}
-func (self *astore_0) Execute(thread *rtda.Thread) {
-    _astore(thread, 0)
+func (self *astore_0) Execute(frame *rtda.Frame) {
+    _astore(frame, 0)
 }
 
 type astore_1 struct {NoOperandsInstruction}
-func (self *astore_1) Execute(thread *rtda.Thread) {
-    _astore(thread, 1)
+func (self *astore_1) Execute(frame *rtda.Frame) {
+    _astore(frame, 1)
 }
 
 type astore_2 struct {NoOperandsInstruction}
-func (self *astore_2) Execute(thread *rtda.Thread) {
-    _astore(thread, 2)
+func (self *astore_2) Execute(frame *rtda.Frame) {
+    _astore(frame, 2)
 }
 
 type astore_3 struct {NoOperandsInstruction}
-func (self *astore_3) Execute(thread *rtda.Thread) {
-    _astore(thread, 3)
+func (self *astore_3) Execute(frame *rtda.Frame) {
+    _astore(frame, 3)
 }
 
-func _astore(thread *rtda.Thread, index uint) {
-    ref := thread.CurrentFrame().OperandStack().PopRef()
-    thread.CurrentFrame().LocalVars().SetRef(index, ref)
+func _astore(frame *rtda.Frame, index uint) {
+    ref := frame.OperandStack().PopRef()
+    frame.LocalVars().SetRef(index, ref)
 }

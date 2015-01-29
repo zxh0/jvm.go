0ef1eb25165b895389974f26020afe6bd381e375
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 16:09:17 +0800

    refactor ldc.go

--- src/jvmgo/instructions/ldc.go
+++ src/jvmgo/instructions/ldc.go
@@ -8,18 +8,17 @@ import (
 
 // Push item from run-time constant pool 
 type ldc struct {Index8Instruction}
-func (self *ldc) Execute(thread *rtda.Thread) {
-    _ldc(thread, self.index)
+func (self *ldc) Execute(frame *rtda.Frame) {
+    _ldc(frame, self.index)
 }
 
 // Push item from run-time constant pool (wide index)
 type ldc_w struct {Index16Instruction}
-func (self *ldc_w) Execute(thread *rtda.Thread) {
-    _ldc(thread, self.index)
+func (self *ldc_w) Execute(frame *rtda.Frame) {
+    _ldc(frame, self.index)
 }
 
-func _ldc(thread *rtda.Thread, index uint) {
-    frame := thread.CurrentFrame()
+func _ldc(frame *rtda.Frame, index uint) {
     stack := frame.OperandStack()
     cp := frame.Method().Class().ConstantPool()
     c := cp.GetConstant(index)
@@ -27,7 +26,7 @@ func _ldc(thread *rtda.Thread, index uint) {
     switch c.(type) {
     case int32: stack.PushInt(c.(int32))
     case float32: stack.PushFloat(c.(float32))
-    case string: newJString(c.(string), thread) // todo
+    case string: newJString(c.(string), frame.Thread()) // todo
     case *rtc.ConstantClass: // todo
         class := c.(*rtc.ConstantClass).Class()
         stack.PushRef(class.JClass())
@@ -43,8 +42,7 @@ func _ldc(thread *rtda.Thread, index uint) {
 
 // Push long or double from run-time constant pool (wide index) 
 type ldc2_w struct {Index16Instruction}
-func (self *ldc2_w) Execute(thread *rtda.Thread) {
-    frame := thread.CurrentFrame()
+func (self *ldc2_w) Execute(frame *rtda.Frame) {
     stack := frame.OperandStack()
     cp := frame.Method().Class().ConstantPool()
     c := cp.GetConstant(self.index)

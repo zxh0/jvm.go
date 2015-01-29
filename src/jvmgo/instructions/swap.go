54e41b4b9353deee02133167fcbbf4aa6984f76f
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:44:48 +0800

    refactor swap

--- src/jvmgo/instructions/swap.go
+++ src/jvmgo/instructions/swap.go
@@ -4,8 +4,8 @@ import "jvmgo/rtda"
 
 // Swap the top two operand stack values
 type swap struct {NoOperandsInstruction}
-func (self *swap) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *swap) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     val1 := stack.Pop()
     val2 := stack.Pop()
     stack.Push(val1)

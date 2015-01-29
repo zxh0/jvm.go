256b45c4814ed159372e1d3bc27113a99f00306a
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:25:20 +0800

    refactor d2x

--- src/jvmgo/instructions/d2x.go
+++ src/jvmgo/instructions/d2x.go
@@ -4,8 +4,8 @@ import "jvmgo/rtda"
 
 // Convert double to float
 type d2f struct {NoOperandsInstruction}
-func (self *d2f) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *d2f) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     d := stack.PopDouble()
     f := float32(d)
     stack.PushFloat(f)
@@ -13,8 +13,8 @@ func (self *d2f) Execute(thread *rtda.Thread) {
 
 // Convert double to int
 type d2i struct {NoOperandsInstruction}
-func (self *d2i) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *d2i) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     d := stack.PopDouble()
     i := int32(d)
     stack.PushInt(i)
@@ -22,8 +22,8 @@ func (self *d2i) Execute(thread *rtda.Thread) {
 
 // Convert double to long
 type d2l struct {NoOperandsInstruction}
-func (self *d2l) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *d2l) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     d := stack.PopDouble()
     l := int64(d)
     stack.PushLong(l)

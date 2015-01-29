8bd945a4b5bbb3afbeca6f179d9b32213fadc047
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:19:16 +0800

    !!!!!!!!!!refactor aload.go!!!!!!!!!!

--- src/jvmgo/instructions/aload.go
+++ src/jvmgo/instructions/aload.go
@@ -4,31 +4,31 @@ import "jvmgo/rtda"
 
 // Load reference from local variable 
 type aload struct {Index8Instruction}
-func (self *aload) Execute(thread *rtda.Thread) {
-    _aload(thread, uint(self.index))
+func (self *aload) Execute(frame *rtda.Frame) {
+    _aload(frame, uint(self.index))
 }
 
 type aload_0 struct {NoOperandsInstruction}
-func (self *aload_0) Execute(thread *rtda.Thread) {
-    _aload(thread, 0)
+func (self *aload_0) Execute(frame *rtda.Frame) {
+    _aload(frame, 0)
 }
 
 type aload_1 struct {NoOperandsInstruction}
-func (self *aload_1) Execute(thread *rtda.Thread) {
-    _aload(thread, 1)
+func (self *aload_1) Execute(frame *rtda.Frame) {
+    _aload(frame, 1)
 }
 
 type aload_2 struct {NoOperandsInstruction}
-func (self *aload_2) Execute(thread *rtda.Thread) {
-    _aload(thread, 2)
+func (self *aload_2) Execute(frame *rtda.Frame) {
+    _aload(frame, 2)
 }
 
 type aload_3 struct {NoOperandsInstruction}
-func (self *aload_3) Execute(thread *rtda.Thread) {
-    _aload(thread, 3)
+func (self *aload_3) Execute(frame *rtda.Frame) {
+    _aload(frame, 3)
 }
 
-func _aload(thread *rtda.Thread, index uint) {
-    ref := thread.CurrentFrame().LocalVars().GetRef(index)
-    thread.CurrentFrame().OperandStack().PushRef(ref)
+func _aload(frame *rtda.Frame, index uint) {
+    ref := frame.LocalVars().GetRef(index)
+    frame.OperandStack().PushRef(ref)
 }

46e12197e54f84b3bb909c758545d553b4e4f85b
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 11:32:29 +0800

    add Class.go

---
+++ src/jvmgo/native/java/lang/Class.go
@@ -0,0 +1,33 @@
+package lang
+
+import (
+    . "jvmgo/any"
+    "jvmgo/rtda"
+    rtc "jvmgo/rtda/class"
+)
+
+func init() {
+    jlClass ("getName0",                "()Ljava/lang/String;",         getName0)
+    jlClass ("getClassLoader0",         "()Ljava/lang/ClassLoader;",    getClassLoader0)
+    jlClass ("desiredAssertionStatus0", "(Ljava/lang/Class;)Z",         desiredAssertionStatus0)
+}
+
+func jlClass(name, desc string, method Any) {
+    rtc.RegisterNativeMethod("java/lang/Class", name, desc, method)
+}
+
+func getName0(stack *rtda.OperandStack) {
+    panic("getName0")
+}
+
+func getClassLoader0(stack *rtda.OperandStack) {
+    // todo
+    _ = stack.PopRef() // this
+    stack.PushRef(nil)
+}
+
+func desiredAssertionStatus0(stack *rtda.OperandStack) {
+    // todo
+    _ = stack.PopRef() // this
+    stack.PushBoolean(false)
+}
--- src/jvmgo/native/java_lang.go
+++ src/jvmgo/native/java_lang.go
@@ -11,9 +11,6 @@ import (
 // register native methods
 func init() {
     rtc.SetRegisterNatives(registerNatives)
-    jlClass     ("getName0",                "()Ljava/lang/String;",         getName0)
-    jlClass     ("getClassLoader0",         "()Ljava/lang/ClassLoader;",    getClassLoader0)
-    jlClass     ("desiredAssertionStatus0", "(Ljava/lang/Class;)Z",         desiredAssertionStatus0)
     jlThrowable ("fillInStackTrace",        "(I)Ljava/lang/Throwable;",     fillInStackTrace)
     // hack!
     rtc.RegisterNativeMethod("jvmgo/SystemOut", "println", "(Ljava/lang/String;)V", jvmgo_SystemOut_println)
@@ -23,26 +20,8 @@ func registerNatives(operandStack *rtda.OperandStack) {
     // todo
 }
 
-func jlClass(name, desc string, method Any) {
-    rtc.RegisterNativeMethod("java/lang/Class", name, desc, method)
-}
 func jlThrowable(name, desc string, method Any) {
     rtc.RegisterNativeMethod("java/lang/Throwable", name, desc, method)
-}
-
-// java.lang.Class
-func getName0(stack *rtda.OperandStack) {
-    panic("getName0")
-}
-func getClassLoader0(stack *rtda.OperandStack) {
-    // todo
-    _ = stack.PopRef() // this
-    stack.PushRef(nil)
-}
-func desiredAssertionStatus0(stack *rtda.OperandStack) {
-    // todo
-    _ = stack.PopRef() // this
-    stack.PushBoolean(false)
 }
 
 // java.lang.Throwable

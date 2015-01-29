fce478d489babcd7f4f94c93a911566009186f91
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:52:29 +0800

    refactor xor

--- src/jvmgo/instructions/xor.go
+++ src/jvmgo/instructions/xor.go
@@ -4,8 +4,8 @@ import "jvmgo/rtda"
 
 // Boolean OR int
 type ior struct {NoOperandsInstruction}
-func (self *ior) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *ior) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopInt()
     v1 := stack.PopInt()
     result := v1 | v2
@@ -14,8 +14,8 @@ func (self *ior) Execute(thread *rtda.Thread) {
 
 // Boolean OR long
 type lor struct {NoOperandsInstruction}
-func (self *lor) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *lor) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopLong()
     v1 := stack.PopLong()
     result := v1 | v2

483a77575706c844b6d449636b225c5814810014
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:53:03 +0800

    refactor xrem

--- src/jvmgo/instructions/xrem.go
+++ src/jvmgo/instructions/xrem.go
@@ -7,8 +7,8 @@ import (
 
 // Remainder double
 type drem struct {NoOperandsInstruction}
-func (self *drem) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *drem) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopDouble()
     v1 := stack.PopDouble()
     result := math.Mod(v1, v2) // todo
@@ -17,8 +17,8 @@ func (self *drem) Execute(thread *rtda.Thread) {
 
 // Remainder float
 type frem struct {NoOperandsInstruction}
-func (self *frem) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *frem) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopFloat()
     v1 := stack.PopFloat()
     result := float32(math.Mod(float64(v1), float64(v2))) // todo
@@ -27,8 +27,8 @@ func (self *frem) Execute(thread *rtda.Thread) {
 
 // Remainder int
 type irem struct {NoOperandsInstruction}
-func (self *irem) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *irem) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopInt()
     v1 := stack.PopInt()
     result := v1 % v2
@@ -37,8 +37,8 @@ func (self *irem) Execute(thread *rtda.Thread) {
 
 // Remainder long
 type lrem struct {NoOperandsInstruction}
-func (self *lrem) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *lrem) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopLong()
     v1 := stack.PopLong()
     result := v1 % v2

375267acb71d15a0fab8486d3cece7357c4c966b
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:50:01 +0800

    refactor xdiv

--- src/jvmgo/instructions/xdiv.go
+++ src/jvmgo/instructions/xdiv.go
@@ -4,8 +4,8 @@ import "jvmgo/rtda"
 
 // Divide double
 type ddiv struct {NoOperandsInstruction}
-func (self *ddiv) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *ddiv) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopDouble()
     v1 := stack.PopDouble()
     result := v1 / v2
@@ -14,8 +14,8 @@ func (self *ddiv) Execute(thread *rtda.Thread) {
 
 // Divide float
 type fdiv struct {NoOperandsInstruction}
-func (self *fdiv) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *fdiv) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopFloat()
     v1 := stack.PopFloat()
     result := v1 / v2
@@ -24,8 +24,8 @@ func (self *fdiv) Execute(thread *rtda.Thread) {
 
 // Divide int
 type idiv struct {NoOperandsInstruction}
-func (self *idiv) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *idiv) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopInt()
     v1 := stack.PopInt()
     result := v1 / v2
@@ -34,8 +34,8 @@ func (self *idiv) Execute(thread *rtda.Thread) {
 
 // Divide long
 type ldiv struct {NoOperandsInstruction}
-func (self *ldiv) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *ldiv) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     v2 := stack.PopLong()
     v1 := stack.PopLong()
     result := v1 / v2

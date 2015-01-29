b16964a67935530ad4c969cc849ec6f232149c31
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:34:46 +0800

    refactor fstore

--- src/jvmgo/instructions/fstore.go
+++ src/jvmgo/instructions/fstore.go
@@ -4,31 +4,31 @@ import "jvmgo/rtda"
 
 // Store float into local variable 
 type fstore struct {Index8Instruction}
-func (self *fstore) Execute(thread *rtda.Thread) {
-    _fstore(thread, uint(self.index))
+func (self *fstore) Execute(frame *rtda.Frame) {
+    _fstore(frame, uint(self.index))
 }
 
 type fstore_0 struct {NoOperandsInstruction}
-func (self *fstore_0) Execute(thread *rtda.Thread) {
-    _fstore(thread, 0)
+func (self *fstore_0) Execute(frame *rtda.Frame) {
+    _fstore(frame, 0)
 }
 
 type fstore_1 struct {NoOperandsInstruction}
-func (self *fstore_1) Execute(thread *rtda.Thread) {
-    _fstore(thread, 1)
+func (self *fstore_1) Execute(frame *rtda.Frame) {
+    _fstore(frame, 1)
 }
 
 type fstore_2 struct {NoOperandsInstruction}
-func (self *fstore_2) Execute(thread *rtda.Thread) {
-    _fstore(thread, 2)
+func (self *fstore_2) Execute(frame *rtda.Frame) {
+    _fstore(frame, 2)
 }
 
 type fstore_3 struct {NoOperandsInstruction}
-func (self *fstore_3) Execute(thread *rtda.Thread) {
-    _fstore(thread, 3)
+func (self *fstore_3) Execute(frame *rtda.Frame) {
+    _fstore(frame, 3)
 }
 
-func _fstore(thread *rtda.Thread, index uint) {
-    val := thread.CurrentFrame().OperandStack().PopFloat()
-    thread.CurrentFrame().LocalVars().SetFloat(index, val)
+func _fstore(frame *rtda.Frame, index uint) {
+    val := frame.OperandStack().PopFloat()
+    frame.LocalVars().SetFloat(index, val)
 }

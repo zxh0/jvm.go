92f3a73c3581d5a2e9f716ec27789ddc7ec59134
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:20:07 +0800

    refactor arraylength

--- src/jvmgo/instructions/arraylength.go
+++ src/jvmgo/instructions/arraylength.go
@@ -7,8 +7,8 @@ import (
 
 // Get length of array
 type arraylength struct {NoOperandsInstruction}
-func (self *arraylength) Execute(thread *rtda.Thread) {
-    stack := thread.CurrentFrame().OperandStack()
+func (self *arraylength) Execute(frame *rtda.Frame) {
+    stack := frame.OperandStack()
     arrRef := stack.PopRef()
     arrLen := class.ArrayLength(arrRef)
     stack.PushInt(arrLen)

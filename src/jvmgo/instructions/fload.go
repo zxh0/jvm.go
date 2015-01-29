ba63034bc42f4785981f0454fb0e1fc274fa0c83
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:33:55 +0800

    refactor fload

--- src/jvmgo/instructions/fload.go
+++ src/jvmgo/instructions/fload.go
@@ -4,31 +4,31 @@ import "jvmgo/rtda"
 
 // Load float from local variable 
 type fload struct {Index8Instruction}
-func (self *fload) Execute(thread *rtda.Thread) {
-    _fload(thread, uint(self.index))
+func (self *fload) Execute(frame *rtda.Frame) {
+    _fload(frame, uint(self.index))
 }
 
 type fload_0 struct {NoOperandsInstruction}
-func (self *fload_0) Execute(thread *rtda.Thread) {
-    _fload(thread, 0)
+func (self *fload_0) Execute(frame *rtda.Frame) {
+    _fload(frame, 0)
 }
 
 type fload_1 struct {NoOperandsInstruction}
-func (self *fload_1) Execute(thread *rtda.Thread) {
-    _fload(thread, 1)
+func (self *fload_1) Execute(frame *rtda.Frame) {
+    _fload(frame, 1)
 }
 
 type fload_2 struct {NoOperandsInstruction}
-func (self *fload_2) Execute(thread *rtda.Thread) {
-    _fload(thread, 2)
+func (self *fload_2) Execute(frame *rtda.Frame) {
+    _fload(frame, 2)
 }
 
 type fload_3 struct {NoOperandsInstruction}
-func (self *fload_3) Execute(thread *rtda.Thread) {
-    _fload(thread, 3)
+func (self *fload_3) Execute(frame *rtda.Frame) {
+    _fload(frame, 3)
 }
 
-func _fload(thread *rtda.Thread, index uint) {
-    val := thread.CurrentFrame().LocalVars().GetFloat(index)
-    thread.CurrentFrame().OperandStack().PushFloat(val)
+func _fload(frame *rtda.Frame, index uint) {
+    val := frame.LocalVars().GetFloat(index)
+    frame.OperandStack().PushFloat(val)
 }

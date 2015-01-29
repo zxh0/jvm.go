38eeca57fbfc9edcd51dc207adfbfacf20c5b992
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-29 15:42:30 +0800

    refactor lstore

--- src/jvmgo/instructions/lstore.go
+++ src/jvmgo/instructions/lstore.go
@@ -4,31 +4,31 @@ import "jvmgo/rtda"
 
 // Store long into local variable 
 type lstore struct {Index8Instruction}
-func (self *lstore) Execute(thread *rtda.Thread) {
-    _lstore(thread, uint(self.index))
+func (self *lstore) Execute(frame *rtda.Frame) {
+    _lstore(frame, uint(self.index))
 }
 
 type lstore_0 struct {NoOperandsInstruction}
-func (self *lstore_0) Execute(thread *rtda.Thread) {
-    _lstore(thread, 0)
+func (self *lstore_0) Execute(frame *rtda.Frame) {
+    _lstore(frame, 0)
 }
 
 type lstore_1 struct {NoOperandsInstruction}
-func (self *lstore_1) Execute(thread *rtda.Thread) {
-    _lstore(thread, 1)
+func (self *lstore_1) Execute(frame *rtda.Frame) {
+    _lstore(frame, 1)
 }
 
 type lstore_2 struct {NoOperandsInstruction}
-func (self *lstore_2) Execute(thread *rtda.Thread) {
-    _lstore(thread, 2)
+func (self *lstore_2) Execute(frame *rtda.Frame) {
+    _lstore(frame, 2)
 }
 
 type lstore_3 struct {NoOperandsInstruction}
-func (self *lstore_3) Execute(thread *rtda.Thread) {
-    _lstore(thread, 3)
+func (self *lstore_3) Execute(frame *rtda.Frame) {
+    _lstore(frame, 3)
 }
 
-func _lstore(thread *rtda.Thread, index uint) {
-    val := thread.CurrentFrame().OperandStack().PopLong()
-    thread.CurrentFrame().LocalVars().SetLong(index, val)
+func _lstore(frame *rtda.Frame, index uint) {
+    val := frame.OperandStack().PopLong()
+    frame.LocalVars().SetLong(index, val)
 }

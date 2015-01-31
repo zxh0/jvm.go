44e8b6047318ac58a343ca64529636ea1d150b81
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-31 11:59:17 +0800

    code refactor

--- src/jvmgo/rtda/class/array.go
+++ src/jvmgo/rtda/class/array.go
@@ -15,13 +15,13 @@ const (
 func NewPrimitiveArray(atype uint8, count int32) (*Obj) {
     switch atype {
     case AT_BOOLEAN: return &Obj{nil, make([]int8, count)}
-    case AT_CHAR:    return &Obj{nil, make([]uint16, count)}
-    case AT_FLOAT:   return &Obj{nil, make([]float32, count)}
-    case AT_DOUBLE:  return &Obj{nil, make([]float64, count)}
     case AT_BYTE:    return &Obj{nil, make([]int8, count)}
+    case AT_CHAR:    return &Obj{nil, make([]uint16, count)}
     case AT_SHORT:   return &Obj{nil, make([]int16, count)}
     case AT_INT:     return &Obj{nil, make([]int32, count)}
     case AT_LONG:    return &Obj{nil, make([]int64, count)}
+    case AT_FLOAT:   return &Obj{nil, make([]float32, count)}
+    case AT_DOUBLE:  return &Obj{nil, make([]float64, count)}
     default: panic("BAD atype!") // todo
     }
 }

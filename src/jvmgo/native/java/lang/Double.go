7d1336a6f9b05293a4e7b898e09891803bcaa7cd
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-31 17:15:22 +0800

    native: java.lang.Double.doubleToRawLongBits()

---
+++ src/jvmgo/native/java/lang/Double.go
@@ -0,0 +1,25 @@
+package lang
+
+import (
+    "math"
+    . "jvmgo/any"
+    "jvmgo/rtda"
+    rtc "jvmgo/rtda/class"
+)
+
+func init() {
+    _double(doubleToRawLongBits, "doubleToRawLongBits", "(D)J")
+}
+
+func _double(method Any, name, desc string) {
+    rtc.RegisterNativeMethod("java/lang/Double", name, desc, method)
+}
+
+// public static native long doubleToRawLongBits(double value);
+// (D)J
+func doubleToRawLongBits(frame *rtda.Frame) {
+    stack := frame.OperandStack()
+    value := stack.PopDouble()
+    bits := math.Float64bits(value)
+    stack.PushLong(int64(bits)) // todo
+}

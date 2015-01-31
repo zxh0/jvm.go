4d51d084bc40fe50151b2508bcb7d0c976014361
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-31 17:38:53 +0800

    native: java.lang.Thread.currentThread()

---
+++ src/jvmgo/native/java/lang/Thread.go
@@ -0,0 +1,23 @@
+package lang
+
+import (
+    . "jvmgo/any"
+    "jvmgo/rtda"
+    rtc "jvmgo/rtda/class"
+)
+
+func init() {
+    _thread(currentThread, "currentThread", "()Ljava/lang/Thread;")
+}
+
+func _thread(method Any, name, desc string) {
+    rtc.RegisterNativeMethod("java/lang/Thread", name, desc, method)
+}
+
+// public static native Thread currentThread();
+// ()Ljava/lang/Thread;
+func currentThread(frame *rtda.Frame) {
+    // stack := frame.OperandStack()
+    // todo
+    panic("currentThread()")
+}

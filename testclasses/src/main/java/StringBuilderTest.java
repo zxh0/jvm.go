f58a64dc1c7cdafcc1aa179fb9cbc79753c26a6d
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-31 10:43:13 +0800

    add StringBuilderTest.java

---
+++ testclasses/src/main/java/StringBuilderTest.java
@@ -0,0 +1,16 @@
+public class StringBuilderTest {
+    
+    public static void main(String[] args) {
+        //String s = "abc" + nextThreadNum();
+        //System.out.println(s);
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("abc");
+    }
+    
+    private static int threadInitNumber;
+    private static synchronized int nextThreadNum() {
+        return threadInitNumber++;
+    }
+    
+}

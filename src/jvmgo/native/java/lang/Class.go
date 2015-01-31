b1c3242bdf7a779ada73117b6b59ca854a2cbb19
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-31 16:19:11 +0800

    reorder code

--- src/jvmgo/native/java/lang/Class.go
+++ src/jvmgo/native/java/lang/Class.go
@@ -7,17 +7,21 @@ import (
 )
 
 func init() {
-    _class("getName0",                  "()Ljava/lang/String;",         getName0)
-    _class("getClassLoader0",           "()Ljava/lang/ClassLoader;",    getClassLoader0)
     _class("desiredAssertionStatus0",   "(Ljava/lang/Class;)Z",         desiredAssertionStatus0)
+    _class("getClassLoader0",           "()Ljava/lang/ClassLoader;",    getClassLoader0)
+    _class("getName0",                  "()Ljava/lang/String;",         getName0)
 }
 
 func _class(name, desc string, method Any) {
     rtc.RegisterNativeMethod("java/lang/Class", name, desc, method)
 }
+//java/lang/Class~getPrimitiveClass~(Ljava/lang/String;)Ljava/lang/Class;
 
-func getName0(frame *rtda.Frame) {
-    panic("getName0")
+func desiredAssertionStatus0(frame *rtda.Frame) {
+    // todo
+    stack := frame.OperandStack()
+    _ = stack.PopRef() // this
+    stack.PushBoolean(false)
 }
 
 func getClassLoader0(frame *rtda.Frame) {
@@ -27,9 +31,6 @@ func getClassLoader0(frame *rtda.Frame) {
     stack.PushRef(nil)
 }
 
-func desiredAssertionStatus0(frame *rtda.Frame) {
-    // todo
-    stack := frame.OperandStack()
-    _ = stack.PopRef() // this
-    stack.PushBoolean(false)
+func getName0(frame *rtda.Frame) {
+    panic("getName0")
 }

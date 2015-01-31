dc9a210dc65e039c6f8dcdab7f8763b1ee123d31
zxh0 <zhangxiuhong2013@gmail.com>
2015-01-31 16:22:34 +0800

    native: java.lang.Class.getPrimitiveClass()

--- src/jvmgo/native/java/lang/Class.go
+++ src/jvmgo/native/java/lang/Class.go
@@ -7,15 +7,15 @@ import (
 )
 
 func init() {
-    _class("desiredAssertionStatus0",   "(Ljava/lang/Class;)Z",         desiredAssertionStatus0)
-    _class("getClassLoader0",           "()Ljava/lang/ClassLoader;",    getClassLoader0)
-    _class("getName0",                  "()Ljava/lang/String;",         getName0)
+    _class("desiredAssertionStatus0",   "(Ljava/lang/Class;)Z",                     desiredAssertionStatus0)
+    _class("getClassLoader0",           "()Ljava/lang/ClassLoader;",                getClassLoader0)
+    _class("getName0",                  "()Ljava/lang/String;",                     getName0)
+    _class("getPrimitiveClass",         "(Ljava/lang/String;)Ljava/lang/Class;",    getPrimitiveClass)
 }
 
 func _class(name, desc string, method Any) {
     rtc.RegisterNativeMethod("java/lang/Class", name, desc, method)
 }
-//java/lang/Class~getPrimitiveClass~(Ljava/lang/String;)Ljava/lang/Class;
 
 func desiredAssertionStatus0(frame *rtda.Frame) {
     // todo
@@ -33,4 +33,11 @@ func getClassLoader0(frame *rtda.Frame) {
 
 func getName0(frame *rtda.Frame) {
     panic("getName0")
+}
+
+//static native Class<?> getPrimitiveClass(String name);
+//getPrimitiveClass(Ljava/lang/String;)Ljava/lang/Class;
+func getPrimitiveClass(frame *rtda.Frame) {
+    // todo
+    panic("getPrimitiveClass")
 }

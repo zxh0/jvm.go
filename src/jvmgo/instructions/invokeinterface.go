22cec6bc2e20d90cddf0e7ad248e7ba3d18ba71c
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-01 14:35:32 +0800

    complete invokeinterface

--- src/jvmgo/instructions/invokeinterface.go
+++ src/jvmgo/instructions/invokeinterface.go
@@ -5,21 +5,38 @@ import (
     //. "jvmgo/any"
     //"jvmgo/native"
     "jvmgo/rtda"
-    //"jvmgo/rtda/class"
+    rtc "jvmgo/rtda/class"
 )
 
 // Invoke interface method
 type invokeinterface struct {
     index   uint16
-    count   uint8
+    count   uint8 // unused
     // 0
 }
+
 func (self *invokeinterface) fetchOperands(bcr *BytecodeReader) {
     self.index = bcr.readUint16()
     self.count = bcr.readUint8()
     bcr.readUint8() // must be 0
 }
+
 func (self *invokeinterface) Execute(frame *rtda.Frame) {
-    // todo
-    panic("todo invokeinterface")
+    thread := frame.Thread()
+    stack := frame.OperandStack()
+    cp := frame.Method().Class().ConstantPool()
+    c := cp.GetConstant(uint(self.index))
+    cMethodRef := c.(*rtc.ConstantInterfaceMethodref)
+    ref := stack.Top(cMethodRef.ArgCount())
+    if ref == nil {
+        panic("NPE")
+    }
+
+    method := cMethodRef.VirtualMethod(ref.(*rtc.Obj))
+    if method.IsNative() {
+        nativeMethod := method.NativeMethod().(func(*rtda.Frame))
+        nativeMethod(frame)
+    } else {
+        thread.InvokeMethod(method)
+    }
 }

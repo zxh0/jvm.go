19bf434d485849785335cd7c9af6f8089d705b8f
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-01 14:57:54 +0800

    add Unsafe.go

---
+++ src/jvmgo/native/sun/misc/Unsafe.go
@@ -0,0 +1,21 @@
+package misc
+
+// import (
+//     . "jvmgo/any"
+//     "jvmgo/rtda"
+//     rtc "jvmgo/rtda/class"
+// )
+
+// func init() {
+//     _unsafe(initialize, "initialize", "()V")
+// }
+
+// func _unsafe(method Any, name, desc string) {
+//     rtc.RegisterNativeMethod("sun/misc/VM", name, desc, method)
+// }
+
+// // private static native void initialize();
+// // ()V
+// func initialize(frame *rtda.Frame) {
+//     // todo
+// }

1d2968a8b71b9ad2c044cc72a5519dfb540457f6
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-01 13:50:20 +0800

    native: java.lang.Runtime.freeMemory()

---
+++ src/jvmgo/native/java/lang/Runtime.go
@@ -0,0 +1,24 @@
+package lang
+
+import (
+    //"math"
+    . "jvmgo/any"
+    "jvmgo/rtda"
+    rtc "jvmgo/rtda/class"
+)
+
+func init() {
+    _runtime(freeMemory, "freeMemory", "()J")
+}
+
+func _runtime(method Any, name, desc string) {
+    rtc.RegisterNativeMethod("java/lang/Runtime", name, desc, method)
+}
+
+// public native long freeMemory();
+// ()J
+func freeMemory(frame *rtda.Frame) {
+    stack := frame.OperandStack()
+    stack.PopRef() // this
+    stack.PushLong(int64(1000000)) // todo
+}

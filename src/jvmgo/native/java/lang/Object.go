b557dec232a2a42d3f4991f8cc872b95f8b09969
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-01 14:41:51 +0800

    native: java.lang.Object.hashCode()

--- src/jvmgo/native/java/lang/Object.go
+++ src/jvmgo/native/java/lang/Object.go
@@ -1,6 +1,7 @@
 package lang
 
 import (
+    "unsafe"
     . "jvmgo/any"
     "jvmgo/rtda"
     rtc "jvmgo/rtda/class"
@@ -8,15 +9,27 @@ import (
 
 func init() {
     _object(getClass, "getClass", "()Ljava/lang/Class;")
+    _object(hashCode, "hashCode", "()I")
 }
 
 func _object(method Any, name, desc string) {
     rtc.RegisterNativeMethod("java/lang/Object", name, desc, method)
 }
 
+// public final native Class<?> getClass();
+// ()Ljava/lang/Class;
 func getClass(frame *rtda.Frame) {
     stack := frame.OperandStack()
     this := stack.PopRef()
     class := this.Class().JClass()
     stack.PushRef(class)
+}
+
+// public native int hashCode();
+// ()I
+func hashCode(frame *rtda.Frame) {
+    stack := frame.OperandStack()
+    this := stack.PopRef()
+    hash := int32(uintptr(unsafe.Pointer(this)))
+    stack.PushInt(hash)
 }

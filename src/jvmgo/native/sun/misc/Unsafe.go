32a784e6997c4fe42ce61f8eb2879b7c29f13e58
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-02 18:40:08 +0800

    native: sun.misc.Unsafe.addressSize()

--- src/jvmgo/native/sun/misc/Unsafe.go
+++ src/jvmgo/native/sun/misc/Unsafe.go
@@ -1,18 +1,29 @@
 package misc
 
 import (
+    //"unsafe"
     . "jvmgo/any"
     "jvmgo/rtda"
     rtc "jvmgo/rtda/class"
 )
 
 func init() {
-    _unsafe(arrayBaseOffset, "arrayBaseOffset", "(Ljava/lang/Class;)I")
-    _unsafe(arrayIndexScale, "arrayIndexScale", "(Ljava/lang/Class;)I")
+    _unsafe(addressSize,        "addressSize",      "()I")
+    _unsafe(arrayBaseOffset,    "arrayBaseOffset",  "(Ljava/lang/Class;)I")
+    _unsafe(arrayIndexScale,    "arrayIndexScale",  "(Ljava/lang/Class;)I")
 }
 
 func _unsafe(method Any, name, desc string) {
     rtc.RegisterNativeMethod("sun/misc/Unsafe", name, desc, method)
+}
+
+// public native int addressSize();
+// ()I
+func addressSize(frame *rtda.Frame) {
+    stack := frame.OperandStack()
+    stack.PopRef() // this
+    //size := unsafe.Sizeof(int)
+    stack.PushInt(0)
 }
 
 // public native int arrayBaseOffset(Class<?> type);

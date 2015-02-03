ee7a970589adb805c88c91a0a0258521e64859f8
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-03 11:51:12 +0800

    test Unsafe

--- testclasses/src/main/java/UnsafeTest.java
+++ testclasses/src/main/java/UnsafeTest.java
@@ -5,20 +5,23 @@ import sun.misc.Unsafe;
 public class UnsafeTest {
     
     public static void main(String[] args) throws Exception {
-        //Unsafe unsafe = Unsafe.getUnsafe();
-        Field f = Unsafe.class.getDeclaredField("theUnsafe");
-        f.setAccessible(true);
-        Unsafe unsafe = (Unsafe) f.get(null);
-
-        System.out.println(unsafe.arrayBaseOffset(new int[0].getClass()));
-        System.out.println(unsafe.arrayBaseOffset(new long[0].getClass()));
-        System.out.println(unsafe.arrayBaseOffset(new Object[0].getClass()));
-        System.out.println(unsafe.arrayBaseOffset(new Class<?>[0].getClass()));
-        
-        System.out.println(unsafe.arrayIndexScale(new int[0].getClass()));
-        System.out.println(unsafe.arrayIndexScale(new long[0].getClass()));
-        System.out.println(unsafe.arrayIndexScale(new Object[0].getClass()));
-        System.out.println(unsafe.arrayIndexScale(new Class<?>[0].getClass()));
+        Unsafe unsafe = Unsafe.getUnsafe();
+//        Field f = Unsafe.class.getDeclaredField("theUnsafe");
+//        f.setAccessible(true);
+//        Unsafe unsafe = (Unsafe) f.get(null);
+//
+//        System.out.println(unsafe.arrayBaseOffset(new int[0].getClass()));
+//        System.out.println(unsafe.arrayBaseOffset(new long[0].getClass()));
+//        System.out.println(unsafe.arrayBaseOffset(new Object[0].getClass()));
+//        System.out.println(unsafe.arrayBaseOffset(new Class<?>[0].getClass()));
+//        
+//        System.out.println(unsafe.arrayIndexScale(new int[0].getClass()));
+//        System.out.println(unsafe.arrayIndexScale(new long[0].getClass()));
+//        System.out.println(unsafe.arrayIndexScale(new Object[0].getClass()));
+//        System.out.println(unsafe.arrayIndexScale(new Class<?>[0].getClass()));
+//        
+//        Class cc = sun.reflect.Reflection.getCallerClass(1);
+//        System.out.println(cc);
     }
     
 }

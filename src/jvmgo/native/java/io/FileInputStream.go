8fb5cb86e95a5786fc9897d4508b1fa7c89404f0
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-04 14:11:15 +0800

    native: java.io.File.initIds()

--- src/jvmgo/native/java/io/FileInputStream.go
+++ src/jvmgo/native/java/io/FileInputStream.go
@@ -1,2 +1,21 @@
 package io
 
+import (
+    . "jvmgo/any"
+    "jvmgo/jvm/rtda"
+    rtc "jvmgo/jvm/rtda/class"
+)
+
+func init() {
+    _fis(initIDs, "initIDs", "()V")
+}
+
+func _fis(method Any, name, desc string) {
+    rtc.RegisterNativeMethod("java/io/FileInputStream", name, desc, method)
+}
+
+// private static native void initIDs();
+// ()V
+func initIDs(frame *rtda.Frame) {
+    // todo
+}

31fdf091f4d6c087bb43c21aecf0c7e1155a3b06
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-04 23:27:09 +0800

    test system properties

--- testclasses/src/main/java/SysProps.java
+++ testclasses/src/main/java/SysProps.java
@@ -6,7 +6,8 @@ public class SysProps {
     public static void main(String[] args) {
         //String val = System.getProperty("key", "val");
         //System.out.println(val);
-        new Properties();
+        //new Properties();
+        System.out.println(System.getProperty("file.encoding"));
     }
     
 }

a7c8ef34865e2289d8ae8f82ff00e80f2e8ebb31
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-04 20:57:17 +0800

    add unit test

--- src/jvmgo/jvm/rtda/jvm_stack_test.go
+++ src/jvmgo/jvm/rtda/jvm_stack_test.go
@@ -2,7 +2,7 @@ package rtda
 
 import (
     "testing"
-    "jvmgo/test"
+    . "jvmgo/test"
 )
 
 func TestPushPop(t *testing.T) {
@@ -15,17 +15,27 @@ func TestPushPop(t *testing.T) {
     //stack.push(_newFrame(0))
 
     for i := maxSize - 1; i >= 0 ; i-- {
-        frame := stack.pop()
-        test.AssertEquals(i, frame.nextPC)
+        AssertEquals(i, stack.pop().nextPC)
     }
+}
+
+func TestTopN(t *testing.T) {
+    stack := newStack(10)
+    stack.push(_newFrame(5))
+    stack.push(_newFrame(6))
+    stack.push(_newFrame(7))
+
+    AssertEquals(7, stack.topN(0).nextPC)
+    AssertEquals(6, stack.topN(1).nextPC)
+    AssertEquals(5, stack.topN(2).nextPC)
 }
 
 func TestIsEmpty(t *testing.T) {
     stack := newStack(17)
-    test.AssertTrue(stack.isEmpty())
+    AssertTrue(stack.isEmpty())
 
     stack.push(_newFrame(1))
-    test.AssertFalse(stack.isEmpty())
+    AssertFalse(stack.isEmpty())
 }
 
 func _newFrame(nextPC int) (*Frame) {

6cbea81e1bcf58437184aa067f6bb651d40a56fb
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-09 11:30:09 +0800

    native: Unsafe. compareAndSwapLong()

--- src/jvmgo/native/sun/misc/Unsafe_atomic.go
+++ src/jvmgo/native/sun/misc/Unsafe_atomic.go
@@ -7,7 +7,8 @@ import (
 )
 
 func init() {
-    _unsafe(compareAndSwapInt,      "compareAndSwapInt",        "(Ljava/lang/Object;JII)Z")
+    _unsafe(compareAndSwapInt,  "compareAndSwapInt",    "(Ljava/lang/Object;JII)Z")
+    _unsafe(compareAndSwapLong, "compareAndSwapLong",   "(Ljava/lang/Object;JJJ)Z")
 }
 
 // public final native boolean compareAndSwapInt(Object o, long offset, int expected, int x);
@@ -24,6 +25,28 @@ func compareAndSwapInt(frame *rtda.Frame) {
     fields := o.Fields().([]Any)
     slot := int(offset)
     actual := fields[slot].(int32)
+    if actual == expected {
+        fields[slot] = x
+        stack.PushBoolean(true)
+    } else {
+        stack.PushBoolean(false)
+    }
+}
+
+// public final native boolean compareAndSwapLong(Object o, long offset, long expected, long x);
+// (Ljava/lang/Object;JJJ)Z
+func compareAndSwapLong(frame *rtda.Frame) {
+    stack := frame.OperandStack()
+    x := stack.PopLong()
+    expected := stack.PopLong()
+    offset := stack.PopLong()
+    o := stack.PopRef()
+    stack.PopRef() // this
+
+    // todo
+    fields := o.Fields().([]Any)
+    slot := int(offset)
+    actual := fields[slot].(int64)
     if actual == expected {
         fields[slot] = x
         stack.PushBoolean(true)

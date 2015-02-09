2f589f6672244878cfad964a4be3af9cfc86da1f
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-09 10:02:15 +0800

    refactor unit tests

--- src/jvmgo/jvm/instructions/instruction_decoder_test.go
+++ src/jvmgo/jvm/instructions/instruction_decoder_test.go
@@ -1,41 +1,32 @@
 package instructions
 
-import "testing"
+import (
+    "testing"
+    . "jvmgo/test"
+)
 
 func TestReadInt8(t *testing.T) {
     bytecodes := []byte{0xf1, 0x17}
     decoder := &InstructionDecoder{0, bytecodes}
-    if b1 := decoder.readInt8(); b1 != -15 {
-        t.Errorf("%v", b1)
-    }
-    if b2 := decoder.readInt8(); b2 != 23 {
-        t.Errorf("%v", b2)
-    }
+    AssertEquals(-15, decoder.readInt8())
+    AssertEquals(23, decoder.readInt8())
 }
 
 func TestReadUint8(t *testing.T) {
     bytecodes := []byte{0xf1, 0x17}
     decoder := &InstructionDecoder{0, bytecodes}
-    if b1 := decoder.readUint8(); b1 != 241 {
-        t.Errorf("%v", b1)
-    }
-    if b2 := decoder.readUint8(); b2 != 23 {
-        t.Errorf("%v", b2)
-    }
+    AssertEquals(241, decoder.readUint8())
+    AssertEquals(23, decoder.readUint8())
 }
 
 func TestReadInt16(t *testing.T) {
     bytecodes := []byte{0xf1, 0x17}
     decoder := &InstructionDecoder{0, bytecodes}
-    if x := decoder.readInt16(); x != -3817 {
-        t.Errorf("%v", x)
-    }
+    AssertEquals(-3817, decoder.readInt16())
 }
 
 func TestReadUint16(t *testing.T) {
     bytecodes := []byte{0xf1, 0x17}
     decoder := &InstructionDecoder{0, bytecodes}
-    if x := decoder.readUint16(); x != 61719 {
-        t.Errorf("%v", x)
-    }
+    AssertEquals(61719, decoder.readUint16())
 }

91d048017a01a4b82a91db0037ff73050adfd189
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-09 11:52:08 +0800

    add Unsafe_class.go

--- src/jvmgo/native/sun/misc/Unsafe.go
+++ src/jvmgo/native/sun/misc/Unsafe.go
@@ -11,9 +11,7 @@ func init() {
     _unsafe(addressSize,            "addressSize",              "()I")
     _unsafe(arrayBaseOffset,        "arrayBaseOffset",          "(Ljava/lang/Class;)I")
     _unsafe(arrayIndexScale,        "arrayIndexScale",          "(Ljava/lang/Class;)I")
-    _unsafe(ensureClassInitialized, "ensureClassInitialized",   "(Ljava/lang/Class;)V")
     _unsafe(objectFieldOffset,      "objectFieldOffset",        "(Ljava/lang/reflect/Field;)J")
-    _unsafe(staticFieldOffset,      "staticFieldOffset",        "(Ljava/lang/reflect/Field;)J")
 }
 
 func _unsafe(method Any, name, desc string) {
@@ -47,38 +45,9 @@ func arrayIndexScale(frame *rtda.Frame) {
     stack.PushInt(1) // todo
 }
 
-// public native void ensureClassInitialized(Class<?> c);
-// (Ljava/lang/Class;)V
-func ensureClassInitialized(frame *rtda.Frame) {
-    stack := frame.OperandStack()
-    classObj := stack.PopRef()
-    this := stack.PopRef()
-
-    goClass := classObj.Extra().(*rtc.Class)
-    if goClass.InitializationNotStarted() {
-        // undo ensureClassInitialized()
-        frame.RevertNextPC()
-        stack.PushRef(this)
-        stack.PushRef(classObj)
-        // init
-        rtda.InitClass(goClass, frame.Thread())
-    }
-}
-
 // public native long objectFieldOffset(Field field);
 // (Ljava/lang/reflect/Field;)J
 func objectFieldOffset(frame *rtda.Frame) {
-    stack := frame.OperandStack()
-    jField := stack.PopRef()
-    stack.PopRef() // this
-
-    offset := jField.GetFieldValue("slot", "I").(int32)
-    stack.PushLong(int64(offset))
-}
-
-// public native long staticFieldOffset(Field field);
-// (Ljava/lang/reflect/Field;)J
-func staticFieldOffset(frame *rtda.Frame) {
     stack := frame.OperandStack()
     jField := stack.PopRef()
     stack.PopRef() // this
---
+++ src/jvmgo/native/sun/misc/Unsafe_class.go
@@ -0,0 +1,42 @@
+package misc
+
+import (
+    //"unsafe"
+    //. "jvmgo/any"
+    "jvmgo/jvm/rtda"
+    rtc "jvmgo/jvm/rtda/class"
+)
+
+func init() {
+    _unsafe(ensureClassInitialized, "ensureClassInitialized",   "(Ljava/lang/Class;)V")
+    _unsafe(staticFieldOffset,      "staticFieldOffset",        "(Ljava/lang/reflect/Field;)J")
+}
+
+// public native void ensureClassInitialized(Class<?> c);
+// (Ljava/lang/Class;)V
+func ensureClassInitialized(frame *rtda.Frame) {
+    stack := frame.OperandStack()
+    classObj := stack.PopRef()
+    this := stack.PopRef()
+
+    goClass := classObj.Extra().(*rtc.Class)
+    if goClass.InitializationNotStarted() {
+        // undo ensureClassInitialized()
+        frame.RevertNextPC()
+        stack.PushRef(this)
+        stack.PushRef(classObj)
+        // init
+        rtda.InitClass(goClass, frame.Thread())
+    }
+}
+
+// public native long staticFieldOffset(Field field);
+// (Ljava/lang/reflect/Field;)J
+func staticFieldOffset(frame *rtda.Frame) {
+    stack := frame.OperandStack()
+    jField := stack.PopRef()
+    stack.PopRef() // this
+
+    offset := jField.GetFieldValue("slot", "I").(int32)
+    stack.PushLong(int64(offset))
+}

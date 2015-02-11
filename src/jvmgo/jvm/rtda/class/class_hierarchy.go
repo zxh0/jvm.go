034633f6dc1c8e24c87cf5b13eed68b4d1e39edb
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-11 18:03:19 +0800

    move methods

--- src/jvmgo/jvm/rtda/class/class_hierarchy.go
+++ src/jvmgo/jvm/rtda/class/class_hierarchy.go
@@ -1,5 +1,32 @@
 package class
 
+// self implements iface
+func (self *Class) isImplements(iface *Class) bool {
+    for k := self; k != nil; k = k.superClass {
+        for _, i := range k.interfaces {
+            if i == iface || i.isSubInterfaceOf(iface) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+// iface extends self
+func (self *Class) isSuperInterfaceOf(iface *Class) bool {
+    return iface.isSubInterfaceOf(self)
+}
+// self extends iface
+func (self *Class) isSubInterfaceOf(iface *Class) bool {
+    for _, superInterface := range self.interfaces {
+        if superInterface == iface || superInterface.isSubInterfaceOf(iface) {
+            return true
+        }
+    }
+    return false
+}
+
+// self extends c
 func (self *Class) isSubClassOf(c *Class) bool {
     for k := self.superClass; k != nil; k = k.superClass {
         if k == c {
@@ -8,6 +35,7 @@ func (self *Class) isSubClassOf(c *Class) bool {
     }
     return false
 }
+// c extends self
 func (self *Class) isSuperClassOf(c *Class) bool {
     return c.isSubClassOf(self)
 }
--- src/jvmgo/jvm/rtda/class/object_instanceof.go
+++ src/jvmgo/jvm/rtda/class/object_instanceof.go
@@ -53,29 +53,3 @@ func (self *Obj) IsInstanceOf(class *Class) (bool) {
     // }
     return false
 }
-
-// self implements iface
-func (self *Class) isImplements(iface *Class) bool {
-    for k := self; k != nil; k = k.superClass {
-        for _, i := range k.interfaces {
-            if i == iface || i.isSubInterfaceOf(iface) {
-                return true
-            }
-        }
-    }
-    return false
-}
-
-// iface extends self
-func (self *Class) isSuperInterfaceOf(iface *Class) bool {
-    return iface.isSubInterfaceOf(self)
-}
-// self extends iface
-func (self *Class) isSubInterfaceOf(iface *Class) bool {
-    for _, superInterface := range self.interfaces {
-        if superInterface == iface || superInterface.isSubInterfaceOf(iface) {
-            return true
-        }
-    }
-    return false
-}

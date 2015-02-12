6b4f070b3383d60af8579d9eac92ae25bcad2bd2
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-12 15:43:04 +0800

    refactor unit test

--- src/jvmgo/classpath/class_path_test.go
+++ src/jvmgo/classpath/class_path_test.go
@@ -3,22 +3,19 @@ package classpath
 import (
     "strings"
     "testing"
+    . "jvmgo/test"
 )
 
 func TestParseClassPath_Empty(t *testing.T) {
     cp := ParseClassPath("")
-    if len(cp.entries) != 0 {
-        t.Error("TestParseClassPath_Empty")
-    }
+    AssertEquals(0, len(cp.entries))
 }
 
 func TestParseClassPath_OneDir(t *testing.T) {
     dirs := []string{".", "abc", "a/b/c"}
     for _, dir := range dirs {
         cp := ParseClassPath(dir)
-        if len(cp.entries) != 1 {
-            t.Error("TestParseClassPath_OneDir")
-        }
+        AssertEquals(1, len(cp.entries))
     }
 }
 
@@ -26,7 +23,5 @@ func TestParseClassPath_List(t *testing.T) {
     pathList := []string{".", "rt.jar"}
     pathStr := strings.Join(pathList, pathListSeparator)
     cp := ParseClassPath(pathStr)
-    if entryCount := len(cp.entries); entryCount != 2 {
-        t.Errorf("entryCount: %v", entryCount)
-    }
+    AssertEquals(2, len(cp.entries))
 }

88f9b266ee11ee271f159db91e351da96a8e1a11
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-14 21:30:23 +0800

    native: Thread.sleep()

--- src/jvmgo/native/java/lang/Thread.go
+++ src/jvmgo/native/java/lang/Thread.go
@@ -1,6 +1,7 @@
 package lang
 
 import (
+    "time"
     . "jvmgo/any"
     "jvmgo/jvm/interpreter"
     "jvmgo/jvm/rtda"
@@ -10,6 +11,7 @@ import (
 func init() {
     _thread(currentThread,  "currentThread",    "()Ljava/lang/Thread;")
     _thread(isAlive,        "isAlive",          "()Z")
+    _thread(sleep,          "sleep",            "(J)V")
     _thread(setPriority0,   "setPriority0",     "(I)V")
     _thread(start0,         "start0",           "()V")
 }
@@ -23,7 +25,6 @@ func _thread(method Any, name, desc string) {
 // public static native boolean holdsLock(Object obj);
 // private native static StackTraceElement[][] dumpThreads(Thread[] threads);
 // private native static Thread[] getThreads();
-// private native void setPriority0(int newPriority);
 // private native void stop0(Object o);
 // private native void suspend0();
 // private native void resume0();
@@ -52,6 +53,17 @@ func setPriority0(frame *rtda.Frame) {
     // this := vars.GetThis()
     // newPriority := vars.GetInt(1))
     // todo
+}
+
+// public static native void sleep(long millis) throws InterruptedException;
+// (J)V
+func sleep(frame *rtda.Frame) {
+    vars := frame.LocalVars()
+    millis := vars.GetLong(0)
+
+    m := millis * int64(time.Millisecond)
+    d := time.Duration(m)
+    time.Sleep(d)
 }
 
 // private native void start0();

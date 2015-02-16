e9396f7de20525d40bf2abfa367a7b84dffc712b
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-16 17:51:10 +0800

    NewAthrowMethod()..

--- src/jvmgo/jvm/rtda/class/shim_methods.go
+++ src/jvmgo/jvm/rtda/class/shim_methods.go
@@ -2,6 +2,7 @@ package class
 
 var _shimClass = &Class{name:"~shim"}
 var _returnCode = []byte{0xb1} // return
+var _athrowCode = []byte{0xbf} // athrow
 
 func NewReturnMethod(maxStack, maxLocals uint) (*Method) {
     method := &Method{}
@@ -14,7 +15,13 @@ func NewReturnMethod(maxStack, maxLocals uint) (*Method) {
     return method
 }
 
-func NewAthrowMethod() (*Method) {
-    // todo
-    return nil
+func NewAthrowMethod(maxStack, maxLocals uint) (*Method) {
+    method := &Method{}
+    method.class = _shimClass
+    method.name = "<athrow>"
+    method.accessFlags = ACC_STATIC
+    method.maxStack = maxStack
+    method.maxLocals = maxLocals
+    method.code = _athrowCode
+    return method
 }

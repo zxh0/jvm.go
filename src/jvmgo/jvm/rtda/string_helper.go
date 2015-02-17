c63724050c74fde81bb5d4acb1a391c8e9f5e608
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-17 18:49:25 +0800

    code refactor

--- src/jvmgo/jvm/rtda/string_helper.go
+++ src/jvmgo/jvm/rtda/string_helper.go
@@ -5,21 +5,15 @@ import (
 	"jvmgo/util"
 )
 
-func JStringChars(jStr *rtc.Obj) []uint16 {
-	charArr := jStr.GetFieldValue("value", "[C").(*rtc.Obj)
-	return charArr.Fields().([]uint16)
-}
-
 // todo: is there a better way to create String?
-// todo: add ClassLoaderGetter interface
-func NewJString(goStr string, frame *Frame) *rtc.Obj {
+func NewJString(goStr string, clg rtc.ClassLoaderGetter) *rtc.Obj {
 	chars := util.StringToUtf16(goStr)
 	internedStr := getInternedString(chars)
 	if internedStr != nil {
 		return internedStr
 	}
 
-	classLoader := frame.ClassLoader()
+	classLoader := clg.ClassLoader()
 	stringClass := classLoader.StringClass()
 	jCharArr := rtc.NewCharArray(chars, classLoader)
 	jStr := stringClass.NewObj()
@@ -30,4 +24,9 @@ func NewJString(goStr string, frame *Frame) *rtc.Obj {
 func GoString(jStr *rtc.Obj) string {
 	utf16 := JStringChars(jStr)
 	return util.Utf16ToString(utf16)
+}
+
+func JStringChars(jStr *rtc.Obj) []uint16 {
+	charArr := jStr.GetFieldValue("value", "[C").(*rtc.Obj)
+	return charArr.Fields().([]uint16)
 }

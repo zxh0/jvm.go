efad42d3aa63cca812aeeb1d6312249b3d83a5b9
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-17 21:50:02 +0800

    merge strings.go and unitcode.go

--- src/jvmgo/util/strings.go
+++ src/jvmgo/util/strings.go
@@ -1,7 +1,22 @@
 package util
 
-import "strings"
+import (
+	"strings"
+	"unicode/utf16"
+)
 
 func ReplaceAll(s, old, _new string) string {
 	return strings.Replace(s, old, _new, -1)
+}
+
+// utf8 -> utf16
+func StringToUtf16(str string) []uint16 {
+	runes := []rune(str)
+	return utf16.Encode(runes) // func Encode(s []rune) []uint16
+}
+
+// utf16 -> utf8
+func Utf16ToString(s []uint16) string {
+	runes := utf16.Decode(s) // func Decode(s []uint16) []rune
+	return string(runes)
 }
--- src/jvmgo/util/strings_test.go
+++ src/jvmgo/util/strings_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestParseCommandFail0(t *testing.T) {
+func TestUtf16(t *testing.T) {
 	str := "abcd1234@@汉字&中国"
 	utf16 := StringToUtf16(str)
 	utf8 := Utf16ToString(utf16)

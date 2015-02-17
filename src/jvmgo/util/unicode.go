c59ddaf9c4a7cd45a43ba40ae33f4cb6ba6ef0ce
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-17 21:47:56 +0800

    simplify code

--- src/jvmgo/util/unicode.go
+++ src/jvmgo/util/unicode.go
@@ -2,41 +2,17 @@ package util
 
 import (
 	"unicode/utf16"
-	"unicode/utf8"
+	// "unicode/utf8"
 )
 
 // utf8 -> utf16
-// todo bytes: func Runes(s []byte) []rune
 func StringToUtf16(str string) []uint16 {
-	runeCount := utf8.RuneCountInString(str)
-	runes := make([]rune, runeCount)
-	i := 0
-	for len(str) > 0 {
-		r, size := utf8.DecodeRuneInString(str)
-		runes[i] = r
-		i++
-		str = str[size:]
-	}
-
-	// func Encode(s []rune) []uint16
-	return utf16.Encode(runes)
+	runes := []rune(str)
+	return utf16.Encode(runes) // func Encode(s []rune) []uint16
 }
 
 // utf16 -> utf8
 func Utf16ToString(s []uint16) string {
-	// func Decode(s []uint16) []rune
-	runes := utf16.Decode(s)
-
-	byteCount := 0
-	for _, r := range runes {
-		byteCount += utf8.RuneLen(r)
-	}
-
-	i := 0
-	bytes := make([]byte, byteCount)
-	for _, r := range runes {
-		i += utf8.EncodeRune(bytes[i:], r)
-	}
-
-	return string(bytes)
+	runes := utf16.Decode(s) // func Decode(s []uint16) []rune
+	return string(runes)
 }
--- src/jvmgo/util/unicode_test.go
+++ src/jvmgo/util/unicode_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestParseCommandFail0(t *testing.T) {
-	str := "abcd1234@@"
+	str := "abcd1234@@汉字&中国"
 	utf16 := StringToUtf16(str)
 	utf8 := Utf16ToString(utf16)
 	AssertSame(str, utf8)

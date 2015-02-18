02ca418b975e7b715d3b50fbddf4393019952ad6
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-18 22:53:30 +0800

    add two methods

--- src/jvmgo/jvm/rtda/class/method_vtable.go
+++ src/jvmgo/jvm/rtda/class/method_vtable.go
@@ -3,3 +3,20 @@ package class
 func createVTable(class *Class) {
 	// todo
 }
+
+func countVirtualMethod(class *Class) int {
+	count := 0
+	for _, m := range class.methods {
+		if isVirtualMethod(m) {
+			count++
+		}
+	}
+	return count
+}
+
+func isVirtualMethod(method *Method) bool {
+	return !method.IsStatic() &&
+		!method.IsFinal() &&
+		!method.IsPrivate() &&
+		method.Name() != constructorName
+}

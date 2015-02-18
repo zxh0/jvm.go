21a8887bf3e9d8f551e61fbaaa87e3a6a5a058f5
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-18 13:47:11 +0800

    refactor unit test

--- src/jvmgo/jvm/rtda/string_pool_test.go
+++ src/jvmgo/jvm/rtda/string_pool_test.go
@@ -2,7 +2,7 @@ package rtda
 
 import (
 	rtc "jvmgo/jvm/rtda/class"
-	"jvmgo/testing"
+	. "jvmgo/testing"
 	"testing"
 )
 
@@ -11,10 +11,10 @@ func TestInternSameString(t *testing.T) {
 	str := &rtc.Obj{}
 
 	str2 := InternString(chars, str)
-	test.AssertSame(str, str2)
+	AssertSame(str, str2)
 
 	str3 := InternString(chars, str2)
-	test.AssertSame(str2, str3)
+	AssertSame(str2, str3)
 }
 
 func TestInternDifferentStrings(t *testing.T) {
@@ -22,11 +22,11 @@ func TestInternDifferentStrings(t *testing.T) {
 	str2, chars2 := &rtc.Obj{}, []uint16{1, 2, 3, 4}
 	str3, chars3 := &rtc.Obj{}, []uint16{1, 2, 3}
 
-	test.AssertSame(str1, InternString(chars1, str1))
-	test.AssertSame(str2, InternString(chars2, str2))
-	test.AssertSame(str3, InternString(chars3, str3))
+	AssertSame(str1, InternString(chars1, str1))
+	AssertSame(str2, InternString(chars2, str2))
+	AssertSame(str3, InternString(chars3, str3))
 
-	test.AssertSame(str1, InternString(chars1, &rtc.Obj{}))
-	test.AssertSame(str2, InternString(chars2, &rtc.Obj{}))
-	test.AssertSame(str3, InternString(chars3, &rtc.Obj{}))
+	AssertSame(str1, InternString(chars1, &rtc.Obj{}))
+	AssertSame(str2, InternString(chars2, &rtc.Obj{}))
+	AssertSame(str3, InternString(chars3, &rtc.Obj{}))
 }

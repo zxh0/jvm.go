ba05359319f659bcd015ce0c41bb2f217d478bcc
zxh0 <zhangxiuhong2013@gmail.com>
2015-02-25 18:02:54 +0800

    native: Runtime. availableProcessors()

--- src/jvmgo/native/java/lang/Runtime.go
+++ src/jvmgo/native/java/lang/Runtime.go
@@ -1,18 +1,28 @@
 package lang
 
 import (
-	//"math"
 	. "jvmgo/any"
 	"jvmgo/jvm/rtda"
 	rtc "jvmgo/jvm/rtda/class"
+	"runtime"
 )
 
 func init() {
+	_runtime(availableProcessors, "availableProcessors", "()I")
 	_runtime(freeMemory, "freeMemory", "()J")
 }
 
 func _runtime(method Any, name, desc string) {
 	rtc.RegisterNativeMethod("java/lang/Runtime", name, desc, method)
+}
+
+// public native int availableProcessors();
+// ()I
+func availableProcessors(frame *rtda.Frame) {
+	numCPU := runtime.NumCPU()
+
+	stack := frame.OperandStack()
+	stack.PushInt(int32(numCPU))
 }
 
 // public native long freeMemory();

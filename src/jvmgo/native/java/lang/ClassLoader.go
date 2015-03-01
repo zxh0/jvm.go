fbaaa67b09ff661abbc6ee1288fa9d4daecaa690
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-01 19:54:10 +0800

    native: ClassLoader. findLoadedClass0()

--- src/jvmgo/native/java/lang/ClassLoader.go
+++ src/jvmgo/native/java/lang/ClassLoader.go
@@ -7,20 +7,25 @@ import (
 )
 
 func init() {
-	_cl(getCaller, "getCaller", "(I)Ljava/lang/Class;")
+	_cl(findLoadedClass0, "findLoadedClass0", "(Ljava/lang/String;)Ljava/lang/Class;")
 }
 
 func _cl(method Any, name, desc string) {
 	rtc.RegisterNativeMethod("java/lang/ClassLoader", name, desc, method)
 }
 
-// private static native Class<?> getCaller(int i)
-// (I)Ljava/lang/Class;
-func getCaller(frame *rtda.Frame) {
+// private native final Class<?> findLoadedClass0(String name);
+// (Ljava/lang/String;)Ljava/lang/Class;
+func findLoadedClass0(frame *rtda.Frame) {
 	vars := frame.LocalVars()
-	i := uint(vars.GetInt(0))
+	//this := vars.GetThis()
+	name := vars.GetRef(1)
 
-	callerClass := frame.Thread().TopFrameN(i).Method().Class().JClass()
-	stack := frame.OperandStack()
-	stack.PushRef(callerClass)
+	className := rtc.NameCfFormat(rtda.GoString(name))
+	class := frame.ClassLoader().LoadClass(className)
+	if class != nil {
+		frame.OperandStack().PushRef(class.JClass())
+	} else {
+		frame.OperandStack().PushRef(nil)
+	}
 }

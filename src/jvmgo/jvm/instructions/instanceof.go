255bae15daa6be848d994d95f0450dd4a3b49798
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-01 13:25:42 +0800

    fix instanceof

--- src/jvmgo/jvm/instructions/instanceof.go
+++ src/jvmgo/jvm/instructions/instanceof.go
@@ -16,11 +16,6 @@ func (self *instanceof) Execute(frame *rtda.Frame) {
 	kClass := cp.GetConstant(self.index).(*rtc.ConstantClass)
 	class := kClass.Class()
 
-	if class.InitializationNotStarted() {
-		// todo: init class
-		panic("class not initialized!" + class.Name())
-	}
-
 	if ref == nil {
 		stack.PushInt(0)
 	} else if ref.IsInstanceOf(class) {

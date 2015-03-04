9b5220e1d2bfe12c505af2f5efac1e5531dd9e74
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-04 16:10:38 +0800

    native method: ConstantPool.getLongAt0()

--- src/jvmgo/native/sun/reflect/ConstantPool.go
+++ src/jvmgo/native/sun/reflect/ConstantPool.go
@@ -7,6 +7,7 @@ import (
 )
 
 func init() {
+	_cp(getLongAt0, "getLongAt0", "(Ljava/lang/Object;I)J")
 	_cp(getUTF8At0, "getUTF8At0", "(Ljava/lang/Object;I)Ljava/lang/String;")
 }
 
@@ -14,16 +15,28 @@ func _cp(method Any, name, desc string) {
 	rtc.RegisterNativeMethod("sun/reflect/ConstantPool", name, desc, method)
 }
 
+// private native long getLongAt0(Object o, int i);
+// (Ljava/lang/Object;I)J
+func getLongAt0(frame *rtda.Frame) {
+	cp, index := _getPop(frame)
+	val := cp.GetConstant(index).(int64)
+	frame.OperandStack().PushLong(val)
+}
+
 // private native String getUTF8At0(Object o, int i);
 // (Ljava/lang/Object;I)Ljava/lang/String;
 func getUTF8At0(frame *rtda.Frame) {
-	vars := frame.LocalVars()
-	this := vars.GetThis()
-	i := uint(vars.GetInt(2))
-
-	cp := this.Extra().(*rtc.ConstantPool)
-	kUtf8 := cp.GetConstant(i).(*rtc.ConstantUtf8)
+	cp, index := _getPop(frame)
+	kUtf8 := cp.GetConstant(index).(*rtc.ConstantUtf8)
 	goStr := kUtf8.Str()
 	jStr := rtda.NewJString(goStr, frame)
 	frame.OperandStack().PushRef(jStr)
+}
+
+func _getPop(frame *rtda.Frame) (cp *rtc.ConstantPool, index uint) {
+	vars := frame.LocalVars()
+	this := vars.GetThis()
+	index = uint(vars.GetInt(2))
+	cp = this.Extra().(*rtc.ConstantPool)
+	return
 }

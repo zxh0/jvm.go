74665043c99f25793ae028a9224c8608631b409a
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-04 12:43:37 +0800

    delete attr_parameter_annotations.go

--- src/jvmgo/classfile/attr_annotations.go
+++ src/jvmgo/classfile/attr_annotations.go
@@ -22,3 +22,26 @@ AnnotationDefault_attribute {
 type AnnotationDefaultAttribute struct {
 	UndefinedAttribute
 }
+
+/*
+RuntimeVisibleParameterAnnotations_attribute {
+    u2 attribute_name_index;
+    u4 attribute_length;
+    u1 num_parameters;
+    {   u2         num_annotations;
+        annotation annotations[num_annotations];
+    } parameter_annotations[num_parameters];
+}
+RuntimeInvisibleParameterAnnotations_attribute {
+    u2 attribute_name_index;
+    u4 attribute_length;
+    u1 num_parameters;
+    {   u2         num_annotations;
+        annotation annotations[num_annotations];
+    } parameter_annotations[num_parameters];
+}
+*/
+
+type ParameterAnnotationsAttribute struct {
+	UndefinedAttribute
+}

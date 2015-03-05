2140ca0962e0dc9c547149968696e2c7d27df2fb
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-05 12:07:23 +0800

    more tests

--- testclasses/src/main/java/reflection/ArrayClassTest.java
+++ testclasses/src/main/java/reflection/ArrayClassTest.java
@@ -21,11 +21,14 @@ public class ArrayClassTest {
         testArrayClass(long[].class,    "[J");
         testArrayClass(float[].class,   "[F");
         testArrayClass(double[].class,  "[D");
+        testArrayClass(int[][].class,   "[[I");
+        testArrayClass(Object[].class,  "[Ljava.lang.Object;");
+        testArrayClass(Object[][].class,"[[Ljava.lang.Object;");
     }
     
     private void testArrayClass(Class<?> c, String name) {
-        assertEquals(Object.class, c.getSuperclass());
         assertEquals(name, c.getName());
+        assertEquals(Object.class, c.getSuperclass());
         assertArrayEquals(new Class<?>[]{Cloneable.class, Serializable.class}, c.getInterfaces());
         assertEquals(0, c.getFields().length);
         assertEquals(0, c.getDeclaredFields().length);

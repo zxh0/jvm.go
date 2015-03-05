aaa16e8d87c13c8681315d9cb6f289b9efcd67c3
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-05 20:37:08 +0800

    convert StringTest to unit test

--- testclasses/src/main/java/StringTest.java
+++ testclasses/src/main/java/StringTest.java
@@ -1,28 +1,25 @@
+import junit.UnitTestRunner;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StringTest {
     
     public static void main(String[] args) {
+        UnitTestRunner.run(StringTest.class);
+    }
+    
+    @Test
+    public void test() {
         String s1 = "abc1";
         String s2 = "abc1";
-        if (s1 == s2) {
-            System.out.println("OK1!");
-        } else {
-            System.out.println("Fail1!");
-        }
+        assertSame(s1, s2);
         
         int x = 1;
         String s3 = "abc" + x;
-        if (s1 != s3) {
-            System.out.println("OK2!");
-        } else {
-            System.out.println("Fail2!");
-        }
+        assertNotSame(s1, s3);
         
         s3 = s3.intern();
-        if (s1 == s3) {
-            System.out.println("OK3!");
-        } else {
-            System.out.println("Fail3!");
-        }
+        assertSame(s1, s3);
     }
     
 }
--- testclasses/src/main/java/UnitTests.java
+++ testclasses/src/main/java/UnitTests.java
@@ -1,14 +1,14 @@
-import junit.framework.Assert;
 import org.junit.Test;
 import reflection.ArrayClassTest;
 import reflection.PrimitiveClassTest;
 import junit.UnitTestRunner;
+import static org.junit.Assert.*;
 
 public class UnitTests {
     
     @Test
     public void test() {
-        Assert.assertEquals(2, 1 + 1);
+        assertEquals(2, 1 + 1);
     }
     
     public static void main(String[] args) {
@@ -16,6 +16,7 @@ public class UnitTests {
             UnitTests.class,
             PrimitiveClassTest.class,
             ArrayClassTest.class,
+            StringTest.class,
         });
     }
     

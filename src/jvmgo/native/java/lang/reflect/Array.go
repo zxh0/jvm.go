4a9be4559b7aaf374111c52bb7bcb635e42a8bf9
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-05 11:10:27 +0800

    native method: Array.getLength()

--- src/jvmgo/native/java/lang/reflect/Array.go
+++ src/jvmgo/native/java/lang/reflect/Array.go
@@ -7,11 +7,24 @@ import (
 )
 
 func init() {
+	_array(getLength, "getLength", "(Ljava/lang/Object;)I")
 	_array(newArray, "newArray", "(Ljava/lang/Class;I)Ljava/lang/Object;")
 }
 
 func _array(method Any, name, desc string) {
 	rtc.RegisterNativeMethod("java/lang/reflect/Array", name, desc, method)
+}
+
+// public static native int getLength(Object array) throws IllegalArgumentException;
+// (Ljava/lang/Object;)I
+func getLength(frame *rtda.Frame) {
+	vars := frame.LocalVars()
+	arr := vars.GetRef(0)
+
+	// todo IllegalArgumentException
+	_len := rtc.ArrayLength(arr)
+	stack := frame.OperandStack()
+	stack.PushInt(_len)
 }
 
 // private static native Object newArray(Class<?> componentType, int length)

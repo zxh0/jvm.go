cec0b8bee7f26a50cbb35ccd7c29e688d5c72ee5
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-06 11:45:16 +0800

    test ClassLoader

--- testclasses/src/main/java/reflection/ClassLoaderTest.java
+++ testclasses/src/main/java/reflection/ClassLoaderTest.java
@@ -8,42 +8,28 @@ public class ClassLoaderTest {
     
     public static void main(String[] args) {
         UnitTestRunner.run(ClassLoaderTest.class);
-        
-
-//        System.out.println(appCl.getParent());
-//        System.out.println(appCl.getParent().getParent());
-        
-//        ClassLoader bootCl = Object.class.getClassLoader();
-//        System.out.println("bootCl:"+bootCl);
-//        ClassLoader appCl = ClassLoaderTest.class.getClassLoader();
-//        System.out.println("appCl:"+appCl);
-//        ClassLoader sysCl = ClassLoader.getSystemClassLoader();
-//        System.out.println("sysCl:"+sysCl);
-//        
-//        System.out.println(Test.class.getClassLoader());
     }
     
     @Test
     public void sysClassLoader() {
         ClassLoader sysCl = ClassLoader.getSystemClassLoader();
         assertEquals("sun.misc.Launcher$AppClassLoader", sysCl.getClass().getName());
+        
         ClassLoader extCl = sysCl.getParent();
         assertEquals("sun.misc.Launcher$ExtClassLoader", extCl.getClass().getName());
+        
         ClassLoader bootCl = extCl.getParent();
         assertNull(bootCl);
     }
     
-    //@Test
-    public void test() {
+    @Test
+    public void getClassLoader() {
         ClassLoader bootCl = Object.class.getClassLoader();
+        assertNull(bootCl);
+        
         ClassLoader appCl = ClassLoaderTest.class.getClassLoader();
         ClassLoader sysCl = ClassLoader.getSystemClassLoader();
-        ClassLoader extCl = appCl.getParent();
-        
-        assertNull(bootCl);
-        assertNotNull(appCl);
-        assertNotNull(extCl);
-        assertSame(appCl, sysCl);
+        assertSame(sysCl, appCl);
     }
     
 }

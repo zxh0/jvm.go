dd5469084df0a7b1b4700ec2e681d51dfe3b6ea0
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-07 09:43:17 +0800

    unit tests

--- testclasses/src/main/java/exception/JvmExTest.java
+++ testclasses/src/main/java/exception/JvmExTest.java
@@ -13,57 +13,73 @@ public class JvmExTest {
     
     @Test(expected = NullPointerException.class)
     public void arraylength() {
-        int[] x = null;
+        int[] x = (int[]) nullObj();
         int y = x.length;
     }
     
-    static void athrow(Exception ex) throws Exception {
-        throw ex;
+    @Test(expected = NullPointerException.class)
+    public void athrow() throws Exception {
+        Exception x = (Exception) nullObj();
+        throw x;
     }
     
-    static void getfield(JvmExTest x) {
+    @Test(expected = NullPointerException.class)
+    public void getfield() {
+        JvmExTest x = (JvmExTest) nullObj();
         int y = x.i;
     }
     
+    @Test(expected = NullPointerException.class)
+    public void monitorenter() {
+        Object x = nullObj();
+        synchronized(x) {
+            System.out.println("BAD!");
+        }
+    }
+    
+    // ClassCastException
     static void checkcast() {
         Object x = "String";
         Integer y = (Integer) x;
     }
     
+    // NegativeArraySizeException
     static void newarray() {
         int[] a = new int[-3];
     }
     
+    // NegativeArraySizeException
     static void anewarray() {
         Object[] a = new Object[-1];
     }
     
+    // ArrayIndexOutOfBoundsException
     static void aload() {
         int[] a = {1};
         int x = a[2];
     }
     
+    // ArrayIndexOutOfBoundsException
     static void astore() {
         int[] a = {};
         a[1] = 2;
         int x = a[1];
     }
     
+    // ArithmeticException
     static void idiv() {
         int x = 0;
         int y = 1 / x;
     }
     
+    // ArithmeticException
     static void irem() {
         int x = 0;
         int y = 1 % x;
     }
     
-    static void monitorenter() {
-        Object x = null;
-        synchronized(x) {
-            System.out.println("BAD!");
-        }
+    private Object nullObj() {
+        return null;
     }
     
 }

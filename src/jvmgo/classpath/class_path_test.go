9246cc36f5d3d5fc18691c353df139555eeb1779
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-07 18:52:21 +0800

    fix unit test

--- src/jvmgo/classpath/class_path_test.go
+++ src/jvmgo/classpath/class_path_test.go
@@ -8,20 +8,20 @@ import (
 
 func TestParseClassPath_Empty(t *testing.T) {
 	cp := ParseClassPath("").compoundEntry
-	AssertEquals(0, len(cp.entries))
+	AssertEquals(2, len(cp.entries))
 }
 
 func TestParseClassPath_OneDir(t *testing.T) {
 	dirs := []string{".", "abc", "a/b/c"}
 	for _, dir := range dirs {
 		cp := ParseClassPath(dir).compoundEntry
-		AssertEquals(1, len(cp.entries))
+		AssertEquals(2, len(cp.entries))
 	}
 }
 
 func TestParseClassPath_List(t *testing.T) {
 	pathList := []string{".", "rt.jar"}
-	pathStr := strings.Join(pathList, pathListSeparator)
+	pathStr := strings.Join(pathList, _pathListSeparator)
 	cp := ParseClassPath(pathStr).compoundEntry
 	AssertEquals(2, len(cp.entries))
 }

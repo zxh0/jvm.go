486073691370727253cb0a32d388ff7b9bb3e95c
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-08 13:57:14 +0800

    hack native method: VMManagementImpl. getStartupTime()

--- src/jvmgo/native/sun/management/VMManagementImpl.go
+++ src/jvmgo/native/sun/management/VMManagementImpl.go
@@ -7,12 +7,23 @@ import (
 )
 
 func init() {
+	_vmm(getStartupTime, "getStartupTime", "()J")
 	_vmm(getVersion0, "getVersion0", "()Ljava/lang/String;")
 	_vmm(initOptionalSupportFields, "initOptionalSupportFields", "()V")
 }
 
 func _vmm(method Any, name, desc string) {
 	rtc.RegisterNativeMethod("sun/management/VMManagementImpl", name, desc, method)
+}
+
+// public native long getStartupTime();
+// ()J
+func getStartupTime(frame *rtda.Frame) {
+	// todo
+	startupTime := int64(0)
+
+	stack := frame.OperandStack()
+	stack.PushLong(startupTime)
 }
 
 // private static native String getVersion0();

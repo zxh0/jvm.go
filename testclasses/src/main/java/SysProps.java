2df31967da82fb4f3ac4b0591a1ba960130b47c5
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-09 16:43:11 +0800

    test system properties

--- testclasses/src/main/java/SysProps.java
+++ testclasses/src/main/java/SysProps.java
@@ -2,15 +2,24 @@ public class SysProps {
     
     public static void main(String[] args) {
         String[] keys = {
-            "user.dir",
+            "java.version",
+            "java.vendor",
+            "java.vendor.url",
             "java.home",
+            "java.class.version",
             "java.class.path",
-            "file.encoding",
-            "sun.stdout.encoding",
-            "sun.stderr.encoding",
+            "os.name",
+            "os.arch",
+            "os.version",
             "file.separator",
             "path.separator",
             "line.separator",
+            "user.name",
+            "user.home",
+            "user.dir",
+            "file.encoding",
+            "sun.stdout.encoding",
+            "sun.stderr.encoding",
         };
         
         for (String key : keys) {

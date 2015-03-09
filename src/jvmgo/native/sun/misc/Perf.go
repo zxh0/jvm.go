74569d88941bd752f096410726e437945638b0ff
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-09 21:40:20 +0800

    hack sun.misc.Perf

---
+++ src/jvmgo/native/sun/misc/Perf.go
@@ -0,0 +1,32 @@
+package misc
+
+import (
+	. "jvmgo/any"
+	"jvmgo/jvm/rtda"
+	rtc "jvmgo/jvm/rtda/class"
+)
+
+func init() {
+	_perf(createLong, "createLong", "(Ljava/lang/String;IIJ)Ljava/nio/ByteBuffer;")
+}
+
+func _perf(method Any, name, desc string) {
+	rtc.RegisterNativeMethod("sun/misc/Perf", name, desc, method)
+}
+
+// public native ByteBuffer createLong(String name, int variability, int units, long value);
+// (Ljava/lang/String;IIJ)Ljava/nio/ByteBuffer;
+func createLong(frame *rtda.Frame) {
+	bbClass := frame.ClassLoader().LoadClass("java/nio/ByteBuffer")
+	if bbClass.InitializationNotStarted() {
+		frame.RevertNextPC()
+		frame.Thread().InitClass(bbClass)
+		return
+	}
+
+	stack := frame.OperandStack()
+	stack.PushInt(8)
+
+	allocate := bbClass.GetStaticMethod("allocate", "(I)Ljava/nio/ByteBuffer;")
+	frame.Thread().InvokeMethod(allocate)
+}

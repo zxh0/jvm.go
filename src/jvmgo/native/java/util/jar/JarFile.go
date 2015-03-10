a1f542ede8b00060dbc06bb5dadf6cf0c59430e6
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-10 12:10:59 +0800

    implement native method: JarFile. getMetaInfEntryNames()

--- src/jvmgo/native/java/util/jar/JarFile.go
+++ src/jvmgo/native/java/util/jar/JarFile.go
@@ -1,5 +1,23 @@
 package jar
 
-func init() {
+import (
+	. "jvmgo/any"
+	"jvmgo/jvm/rtda"
+	rtc "jvmgo/jvm/rtda/class"
+)
 
+func init() {
+	_jf(getMetaInfEntryNames, "getMetaInfEntryNames", "()[Ljava/lang/String;")
+}
+
+func _jf(method Any, name, desc string) {
+	rtc.RegisterNativeMethod("java/util/jar/JarFile", name, desc, method)
+}
+
+// private native String[] getMetaInfEntryNames();
+// ()[Ljava/lang/String;
+func getMetaInfEntryNames(frame *rtda.Frame) {
+	// todo
+	stack := frame.OperandStack()
+	stack.PushNull()
 }

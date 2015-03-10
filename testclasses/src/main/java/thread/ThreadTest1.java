4527404177d7645607a9191ac166bbebdc574972
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-10 19:48:38 +0800

    refactor test

--- testclasses/src/main/java/thread/ThreadTest1.java
+++ testclasses/src/main/java/thread/ThreadTest1.java
@@ -3,16 +3,18 @@ package thread;
 public class ThreadTest1 implements Runnable {
     
     public static void main(String[] args) {
-        new Thread(new ThreadTest1()).start();
-        for (int i = 0; i < 10; i++) {
-            System.out.println("main");
+        Thread t = new Thread(new ThreadTest1());
+        t.start();
+        
+        for (int i = 0; i < 100; i++) {
+            System.out.println("main:" + i);
         }
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("run!");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("run:" + i);
         }
     }
     

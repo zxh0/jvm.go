60b192e31ed2613bb1116015ff4ba03012d34531
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-11 22:57:01 +0800

    catch bug: vtable

--- testclasses/src/main/java/jvmgo/instructions/InvokeTest.java
+++ testclasses/src/main/java/jvmgo/instructions/InvokeTest.java
@@ -1,13 +1,21 @@
 package jvmgo.instructions;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import jvmgo.UnitTestRunner;
 import org.junit.Test;
+import org.junit.runners.model.FrameworkField;
+import org.junit.runners.model.FrameworkMember;
+import org.junit.runners.model.FrameworkMethod;
 import static org.junit.Assert.*;
 
 @SuppressWarnings("serial")
 public class InvokeTest extends ArrayList<String> {
+    
+    public int x;
 
     public static void main(String[] args) {
         UnitTestRunner.run(InvokeTest.class);
@@ -37,6 +45,23 @@ public class InvokeTest extends ArrayList<String> {
         // invokeinterface
         List<String> list = test;
         assertEquals("InvokeTest", list.get(0));
+    }
+    
+    @Test
+    public void invokeVirtual() throws Exception {
+        Constructor<FrameworkField> c = FrameworkField.class.getDeclaredConstructor(Field.class);
+        c.setAccessible(true);
+        Field f = InvokeTest.class.getField("x");
+        FrameworkField ff = c.newInstance(f);
+        
+        Method m = InvokeTest.class.getMethod("invokeVirtual");
+        FrameworkMethod fm = new FrameworkMethod(m);
+        
+        FrameworkMember<?> a = ff;
+        FrameworkMember<?> b = fm;
+        
+        a.getAnnotations();
+        b.getAnnotations();
     }
     
 }

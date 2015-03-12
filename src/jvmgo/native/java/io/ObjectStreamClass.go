a04974e384ffaf4665f6ef1d1118a68992806d59
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-12 09:38:46 +0800

    add ObjectStreamClass.go

---
+++ src/jvmgo/native/java/io/ObjectStreamClass.go
@@ -0,0 +1,21 @@
+package io
+
+import (
+	. "jvmgo/any"
+	"jvmgo/jvm/rtda"
+	rtc "jvmgo/jvm/rtda/class"
+)
+
+func init() {
+	_osc(initNative, "initNative", "()V")
+}
+
+func _osc(method Any, name, desc string) {
+	rtc.RegisterNativeMethod("java/io/ObjectStreamClass", name, desc, method)
+}
+
+// private static native void initNative();
+// ()V
+func initNative(frame *rtda.Frame) {
+	// todo
+}

1d6fe3aa683280464d1ee9dfc97b6fb09316dede
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-12 23:37:02 +0800

    simplify code

--- src/jvmgo/jvm/instructions/invokestatic.go
+++ src/jvmgo/jvm/instructions/invokestatic.go
@@ -1,7 +1,6 @@
 package instructions
 
 import (
-	//"log"
 	"jvmgo/jvm/rtda"
 	rtc "jvmgo/jvm/rtda/class"
 )
@@ -10,23 +9,17 @@ import (
 type invokestatic struct{ Index16Instruction }
 
 func (self *invokestatic) Execute(frame *rtda.Frame) {
-	thread := frame.Thread()
-	currentFrame := frame
-	currentMethod := currentFrame.Method()
-	currentClass := currentMethod.Class()
-	cp := currentClass.ConstantPool()
+	cp := frame.Method().Class().ConstantPool()
 	kMethodRef := cp.GetConstant(self.index).(*rtc.ConstantMethodref)
 	method := kMethodRef.StaticMethod()
 
 	// init class
-	classOfMethod := method.Class()
-	if classOfMethod.InitializationNotStarted() {
-		if classOfMethod != currentClass || !currentMethod.IsClinit() {
-			currentFrame.SetNextPC(thread.PC())
-			thread.InitClass(classOfMethod)
-			return
-		}
+	class := method.Class()
+	if class.InitializationNotStarted() {
+		frame.RevertNextPC()
+		frame.Thread().InitClass(class)
+		return
 	}
 
-	thread.InvokeMethod(method)
+	frame.Thread().InvokeMethod(method)
 }

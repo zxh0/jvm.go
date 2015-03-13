0230fcfe0fcad91844b49df2c0cfd8a66448a8dc
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-13 15:38:19 +0800

    test google gson

---
+++ testclasses/src/main/java/libs/gson/GsonTest.java
@@ -0,0 +1,27 @@
+package libs.gson;
+
+import com.google.gson.Gson;
+import java7.UnitTestRunner;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GsonTest {
+    
+    public int x;
+    public float y;
+    public String z;
+    
+    public static void main(String[] args) {
+        UnitTestRunner.run(GsonTest.class);
+    }
+    
+    @Test
+    public void gson() {
+        GsonTest obj = new GsonTest();
+        obj.x = 100;
+        obj.y = 3.14f;
+        obj.z = "hello";
+        assertEquals("{\"x\":100,\"y\":3.14,\"z\":\"hello\"}", new Gson().toJson(obj));
+    }
+    
+}

1411a7108c57879bb584d9138147702a4444c97f
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-14 21:08:27 +0800

    split Thread.go

--- src/jvmgo/native/java/lang/Thread.go
+++ src/jvmgo/native/java/lang/Thread.go
@@ -5,13 +5,10 @@ import (
 	"jvmgo/jvm/interpreter"
 	"jvmgo/jvm/rtda"
 	rtc "jvmgo/jvm/rtda/class"
-	"time"
 )
 
 func init() {
-	_thread(currentThread, "currentThread", "()Ljava/lang/Thread;")
 	_thread(isAlive, "isAlive", "()Z")
-	_thread(sleep, "sleep", "(J)V")
 	_thread(setPriority0, "setPriority0", "(I)V")
 	_thread(start0, "start0", "()V")
 }
@@ -22,22 +19,11 @@ func _thread(method Any, name, desc string) {
 
 // @Deprecated public native int countStackFrames();
 // private native boolean isInterrupted(boolean ClearInterrupted);
-// public static native boolean holdsLock(Object obj);
-// private native static StackTraceElement[][] dumpThreads(Thread[] threads);
-// private native static Thread[] getThreads();
 // private native void stop0(Object o);
 // private native void suspend0();
 // private native void resume0();
 // private native void interrupt0();
 // private native void setNativeName(String name);
-// public static native void yield();
-
-// public static native Thread currentThread();
-// ()Ljava/lang/Thread;
-func currentThread(frame *rtda.Frame) {
-	jThread := frame.Thread().JThread()
-	frame.OperandStack().PushRef(jThread)
-}
 
 // public final native boolean isAlive();
 // ()Z
@@ -71,17 +57,6 @@ func setPriority0(frame *rtda.Frame) {
 	// this := vars.GetThis()
 	// newPriority := vars.GetInt(1))
 	// todo
-}
-
-// public static native void sleep(long millis) throws InterruptedException;
-// (J)V
-func sleep(frame *rtda.Frame) {
-	vars := frame.LocalVars()
-	millis := vars.GetLong(0)
-
-	m := millis * int64(time.Millisecond)
-	d := time.Duration(m)
-	time.Sleep(d)
 }
 
 // private native void start0();
---
+++ src/jvmgo/native/java/lang/Thread_static.go
@@ -0,0 +1,34 @@
+package lang
+
+import (
+	"jvmgo/jvm/rtda"
+	"time"
+)
+
+func init() {
+	_thread(currentThread, "currentThread", "()Ljava/lang/Thread;")
+	_thread(sleep, "sleep", "(J)V")
+}
+
+// public static native boolean holdsLock(Object obj);
+// public static native void yield();
+// private native static StackTraceElement[][] dumpThreads(Thread[] threads);
+// private native static Thread[] getThreads();
+
+// public static native Thread currentThread();
+// ()Ljava/lang/Thread;
+func currentThread(frame *rtda.Frame) {
+	jThread := frame.Thread().JThread()
+	frame.OperandStack().PushRef(jThread)
+}
+
+// public static native void sleep(long millis) throws InterruptedException;
+// (J)V
+func sleep(frame *rtda.Frame) {
+	vars := frame.LocalVars()
+	millis := vars.GetLong(0)
+
+	m := millis * int64(time.Millisecond)
+	d := time.Duration(m)
+	time.Sleep(d)
+}

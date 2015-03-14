f82cbdc12678ac5e4e229b59c66c2805e59220a8
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-14 19:32:02 +0800

    implement native method: ClassLoader. findBootstrapClass()

--- src/jvmgo/native/java/lang/ClassLoader.go
+++ src/jvmgo/native/java/lang/ClassLoader.go
@@ -7,11 +7,32 @@ import (
 )
 
 func init() {
+	_cl(findBootstrapClass, "findBootstrapClass", "(Ljava/lang/String;)Ljava/lang/Class;")
 	_cl(findLoadedClass0, "findLoadedClass0", "(Ljava/lang/String;)Ljava/lang/Class;")
 }
 
 func _cl(method Any, name, desc string) {
 	rtc.RegisterNativeMethod("java/lang/ClassLoader", name, desc, method)
+}
+
+// private native Class<?> findBootstrapClass(String name);
+// (Ljava/lang/String;)Ljava/lang/Class;
+func findBootstrapClass(frame *rtda.Frame) {
+	vars := frame.LocalVars()
+	//this := vars.GetThis()
+	name := vars.GetRef(1)
+
+	className := rtc.DotToSlash(rtda.GoString(name))
+	class := rtc.BootLoader().LoadClass(className)
+
+	// todo: init class?
+	stack := frame.OperandStack()
+	stack.PushRef(class.JClass())
+
+	// todo
+	if r := recover(); r != nil {
+		frame.OperandStack().PushRef(nil)
+	}
 }
 
 // private native final Class<?> findLoadedClass0(String name);
@@ -21,9 +42,7 @@ func findLoadedClass0(frame *rtda.Frame) {
 	this := vars.GetThis()
 	name := vars.GetRef(1)
 
-	// todo
-	className := rtda.GoString(name)
-	className = rtc.DotToSlash(className)
+	className := rtc.DotToSlash(rtda.GoString(name))
 
 	if isAppClassLoader(this) {
 		class := rtc.BootLoader().FindLoadedClass(className)

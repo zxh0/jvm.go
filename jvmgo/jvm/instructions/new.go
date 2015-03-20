3621055d68b7fbb2ea20a09eff0d9855a009779e
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-20 14:32:43 +0800

    code optimization

--- jvmgo/jvm/instructions/new.go
+++ jvmgo/jvm/instructions/new.go
@@ -6,18 +6,25 @@ import (
 )
 
 // Create new object
-type new_ struct{ Index16Instruction }
+type new_ struct {
+	Index16Instruction
+	class *rtc.Class
+}
 
 func (self *new_) Execute(frame *rtda.Frame) {
-	cp := frame.ConstantPool()
-	kClass := cp.GetConstant(self.index).(*rtc.ConstantClass)
-	class := kClass.Class()
-
-	if class.InitializationNotStarted() {
-		frame.RevertNextPC() // undo new
-		frame.Thread().InitClass(class)
-	} else {
-		ref := class.NewObj()
-		frame.OperandStack().PushRef(ref)
+	if self.class == nil {
+		cp := frame.ConstantPool()
+		kClass := cp.GetConstant(self.index).(*rtc.ConstantClass)
+		self.class = kClass.Class()
 	}
+
+	// init class
+	if self.class.InitializationNotStarted() {
+		frame.RevertNextPC() // undo new
+		frame.Thread().InitClass(self.class)
+		return
+	}
+
+	ref := self.class.NewObj()
+	frame.OperandStack().PushRef(ref)
 }

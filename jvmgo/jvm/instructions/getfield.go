a92817a58cda836ee7d3484a20307753973027e8
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-20 13:24:32 +0800

    move getstatic into it's own file

--- jvmgo/jvm/instructions/getfield.go
+++ jvmgo/jvm/instructions/getfield.go
@@ -28,27 +28,3 @@ func (self *getfield) Execute(frame *rtda.Frame) {
 	val := self.field.GetValue(ref)
 	stack.Push(val)
 }
-
-// Get static field from class
-type getstatic struct {
-	Index16Instruction
-	field *rtc.Field
-}
-
-func (self *getstatic) Execute(frame *rtda.Frame) {
-	if self.field == nil {
-		cp := frame.Method().Class().ConstantPool()
-		kFieldRef := cp.GetConstant(self.index).(*rtc.ConstantFieldref)
-		self.field = kFieldRef.StaticField()
-	}
-
-	class := self.field.Class()
-	if class.InitializationNotStarted() {
-		frame.RevertNextPC() // undo getstatic
-		frame.Thread().InitClass(class)
-		return
-	}
-
-	val := self.field.GetStaticValue()
-	frame.OperandStack().Push(val)
-}
---
+++ jvmgo/jvm/instructions/getstatic.go
@@ -0,0 +1,30 @@
+package instructions
+
+import (
+	"github.com/zxh0/jvm.go/jvmgo/jvm/rtda"
+	rtc "github.com/zxh0/jvm.go/jvmgo/jvm/rtda/class"
+)
+
+// Get static field from class
+type getstatic struct {
+	Index16Instruction
+	field *rtc.Field
+}
+
+func (self *getstatic) Execute(frame *rtda.Frame) {
+	if self.field == nil {
+		cp := frame.Method().Class().ConstantPool()
+		kFieldRef := cp.GetConstant(self.index).(*rtc.ConstantFieldref)
+		self.field = kFieldRef.StaticField()
+	}
+
+	class := self.field.Class()
+	if class.InitializationNotStarted() {
+		frame.RevertNextPC() // undo getstatic
+		frame.Thread().InitClass(class)
+		return
+	}
+
+	val := self.field.GetStaticValue()
+	frame.OperandStack().Push(val)
+}

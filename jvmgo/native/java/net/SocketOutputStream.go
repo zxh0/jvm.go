07b704b20e8ab40552cf9839fd3e91ea5d96c490
Richard Yang <beyondblog@outlook.com>
2015-03-24 09:54:28 +0800

    clear code

--- jvmgo/native/java/net/SocketOutputStream.go
+++ jvmgo/native/java/net/SocketOutputStream.go
@@ -1,7 +1,6 @@
 package io
 
 import (
-	"fmt"
 	. "github.com/zxh0/jvm.go/jvmgo/any"
 	"github.com/zxh0/jvm.go/jvmgo/jvm/rtda"
 	rtc "github.com/zxh0/jvm.go/jvmgo/jvm/rtda/class"
@@ -35,7 +34,6 @@ func sos_socketWrite0(frame *rtda.Frame) {
 	length := vars.GetInt(4)
 
 	conn := fd.Extra().(net.Conn)
-	fmt.Println("########################")
 
 	jBytes := b.Fields().([]int8)
 	jBytes = jBytes[offset : offset+length]

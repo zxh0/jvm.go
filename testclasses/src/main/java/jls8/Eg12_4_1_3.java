1be775f831c03302ba8beb24cc7af4306a041965
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-25 15:22:30 +0800

    add Eg12_4_1_3.java

---
+++ testclasses/src/main/java/jls8/Eg12_4_1_3.java
@@ -0,0 +1,30 @@
+package jls8;
+
+/**
+ * Example 12.4.1-3.
+ * Interface Initialization Does Not Initialize Superinterfaces
+ */
+public class Eg12_4_1_3 {
+    
+    private static interface I {
+        int i = 1, ii = Test.out("ii", 2);
+    }
+    private static interface J extends I {
+        int j = Test.out("j", 3), jj = Test.out("jj", 4);
+    }
+    private static interface K extends J {
+        int k = Test.out("k", 5);
+    }
+    private static class Test {
+        static int out(String s, int i) {
+            System.out.println(s + "=" + i);
+            return i;
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(J.i);
+        System.out.println(K.j);
+    }
+    
+}

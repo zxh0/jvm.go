29446d0ff76af1fe075a531b9f4031fd8f0e35e6
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-25 13:35:44 +0800

    add EmptySlot and NewRefSlot

--- jvmgo/jvm/rtda/class/slot.go
+++ jvmgo/jvm/rtda/class/slot.go
@@ -1,6 +1,12 @@
 package class
 
+var EmptySlot = Slot{0, nil}
+
 type Slot struct {
 	Val int64 // big enough to hold any primitive value
 	Ref *Obj
+}
+
+func NewRefSlot(ref *Obj) Slot {
+	return Slot{0, ref}
 }

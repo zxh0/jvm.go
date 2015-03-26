2cd6622a6e76c1919bfc950f0d49ab0e65d4058a
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-26 20:53:16 +0800

    move func

--- jvmgo/jvm/rtda/string_helper.go
+++ jvmgo/jvm/rtda/string_helper.go
@@ -2,7 +2,7 @@ package rtda
 
 import (
 	rtc "github.com/zxh0/jvm.go/jvmgo/jvm/rtda/class"
-	"github.com/zxh0/jvm.go/jvmgo/util"
+	"unicode/utf16"
 )
 
 // todo: is there a better way to create String?
@@ -12,7 +12,7 @@ func JString(goStr string) *rtc.Obj {
 		return internedStr
 	}
 
-	chars := util.StringToUtf16(goStr)
+	chars := _stringToUtf16(goStr)
 	charArr := rtc.NewCharArray(chars)
 	jStr := rtc.BootLoader().JLStringClass().NewObj()
 	jStr.SetFieldValue("value", "[C", charArr)
@@ -21,5 +21,17 @@ func JString(goStr string) *rtc.Obj {
 
 func GoString(jStr *rtc.Obj) string {
 	charArr := jStr.GetFieldValue("value", "[C").(*rtc.Obj)
-	return util.Utf16ToString(charArr.Chars())
+	return _utf16ToString(charArr.Chars())
+}
+
+// utf8 -> utf16
+func _stringToUtf16(s string) []uint16 {
+	runes := []rune(s)
+	return utf16.Encode(runes) // func Encode(s []rune) []uint16
+}
+
+// utf16 -> utf8
+func _utf16ToString(s []uint16) string {
+	runes := utf16.Decode(s) // func Decode(s []uint16) []rune
+	return string(runes)
 }

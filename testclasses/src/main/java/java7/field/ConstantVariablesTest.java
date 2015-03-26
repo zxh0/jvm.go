89dfe78e6382bb394454e26293b707e71e86dfa5
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-26 16:08:02 +0800

    add more asserts

--- testclasses/src/main/java/java7/field/ConstantVariablesTest.java
+++ testclasses/src/main/java/java7/field/ConstantVariablesTest.java
@@ -8,23 +8,42 @@ import static org.junit.Assert.*;
 // that is initialized with a constant expression (§15.28).
 public class ConstantVariablesTest {
     
+    public static final boolean z = true;
     public static final byte b = 125;
     public static final char c = 'c';
     public static final short s = 300;
     public static final int x = 100;
-    public static final int y = x + 13;
+    public static final int y = x + 18;
     public static final long j = 1L;
     public static final float f = 3.14f;
     public static final double d = 2.71828;
     public static final String str1 = "hello";
-    public static final String str2 = str1 + "world";
+    public static final String str2 = str1 + " world!";
     
     @Test
     public void test() {
+        assertEquals(true, ConstantVariablesTest.z);
+        assertEquals(true, getFieldValue("z"));
         assertEquals((byte)125, ConstantVariablesTest.b);
         assertEquals((byte)125, getFieldValue("b"));
-        
-        
+        assertEquals('c', ConstantVariablesTest.c);
+        assertEquals('c', getFieldValue("c"));
+        assertEquals((short)300, ConstantVariablesTest.s);
+        assertEquals((short)300, getFieldValue("s"));
+        assertEquals(100, ConstantVariablesTest.x);
+        assertEquals(100, getFieldValue("x"));
+        assertEquals(118, ConstantVariablesTest.y);
+        assertEquals(118, getFieldValue("y"));
+        assertEquals(1L, ConstantVariablesTest.j);
+        assertEquals(1L, getFieldValue("j"));
+        assertEquals(3.14f, ConstantVariablesTest.f, 0.1);
+        assertEquals(3.14f, getFieldValue("f"));
+        assertEquals(2.71828, ConstantVariablesTest.d, 0.1);
+        assertEquals(2.71828, getFieldValue("d"));
+        assertEquals("hello", ConstantVariablesTest.str1);
+        assertEquals("hello", getFieldValue("str1"));
+        assertEquals("hello world!", ConstantVariablesTest.str2);
+        assertEquals("hello world!", getFieldValue("str2"));
     }
     
     private static Object getFieldValue(String name) {

7498673030f17c48330081cedf91af486c746353
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-27 09:49:18 +0800

    move native methods

--- jvmgo/native/sun/misc/Unsafe_object.go
+++ jvmgo/native/sun/misc/Unsafe_object.go
@@ -7,6 +7,9 @@ import (
 )
 
 func init() {
+	_unsafe(arrayBaseOffset, "arrayBaseOffset", "(Ljava/lang/Class;)I")
+	_unsafe(arrayIndexScale, "arrayIndexScale", "(Ljava/lang/Class;)I")
+	_unsafe(objectFieldOffset, "objectFieldOffset", "(Ljava/lang/reflect/Field;)J")
 	_unsafe(putObject, "putObject", "(Ljava/lang/Object;JLjava/lang/Object;)V")
 	_unsafe(getObject, "getObject", "(Ljava/lang/Object;J)Ljava/lang/Object;")
 	_unsafe(putObjectVolatile, "putObjectVolatile", "(Ljava/lang/Object;JLjava/lang/Object;)V")
@@ -15,6 +18,39 @@ func init() {
 	_unsafe(getOrderedObject, "getOrderedObject", "(Ljava/lang/Object;J)Ljava/lang/Object;")
 	_unsafe(getIntVolatile, "getIntVolatile", "(Ljava/lang/Object;J)I")
 	_unsafe(getLongVolatile, "getLongVolatile", "(Ljava/lang/Object;J)J")
+}
+
+// public native int arrayBaseOffset(Class<?> type);
+// (Ljava/lang/Class;)I
+func arrayBaseOffset(frame *rtda.Frame) {
+	vars := frame.LocalVars()
+	vars.GetRef(0) // this
+	vars.GetRef(1) // type
+
+	stack := frame.OperandStack()
+	stack.PushInt(0) // todo
+}
+
+// public native int arrayIndexScale(Class<?> type);
+// (Ljava/lang/Class;)I
+func arrayIndexScale(frame *rtda.Frame) {
+	//stack.PopRef() // type
+	//stack.PopRef() // this
+
+	stack := frame.OperandStack()
+	stack.PushInt(1) // todo
+}
+
+// public native long objectFieldOffset(Field field);
+// (Ljava/lang/reflect/Field;)J
+func objectFieldOffset(frame *rtda.Frame) {
+	vars := frame.LocalVars()
+	// this := vars.GetRef(0)
+	jField := vars.GetRef(1)
+
+	offset := jField.GetFieldValue("slot", "I").(int32)
+	stack := frame.OperandStack()
+	stack.PushLong(int64(offset))
 }
 
 // public native void putObject(Object o, long offset, Object x);

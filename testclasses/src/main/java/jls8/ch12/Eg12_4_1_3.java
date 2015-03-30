afb6f76040fc7bc20f3df3f5c12a95fe258f9410
zxh0 <zhangxiuhong2013@gmail.com>
2015-03-30 08:48:44 +0800

    turn Eg12_4_1_3 into unit test

--- testclasses/src/main/java/jls8/ch12/Eg12_4_1_3.java
+++ testclasses/src/main/java/jls8/ch12/Eg12_4_1_3.java
@@ -1,10 +1,16 @@
 package jls8.ch12;
 
+import libs.junit.UnitTestRunner;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Example 12.4.1-3.
  * Interface Initialization Does Not Initialize Superinterfaces
  */
 public class Eg12_4_1_3 {
+    
+    private static String output = "";
     
     private static interface I {
         int i = 1, ii = out("ii", 2);
@@ -15,14 +21,26 @@ public class Eg12_4_1_3 {
     private static interface K extends J {
         int k = out("k", 5);
     }
+    
     static int out(String s, int i) {
-        System.out.println(s + "=" + i);
+        println(s + "=" + i);
         return i;
     }
     
+    private static void println(String s) {
+        output += s;
+        output += "\n";
+    }
+    
+    @Test
+    public void test() {
+        println("" + J.i);
+        println("" + K.j);
+        assertEquals("1\nj=3\njj=4\n3\n", output);
+    }
+    
     public static void main(String[] args) {
-        System.out.println(J.i);
-        System.out.println(K.j);
+        UnitTestRunner.run(Eg12_4_1_3.class);
     }
     
 }

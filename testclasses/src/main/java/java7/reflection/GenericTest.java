10627e8db88643b80f7974b1d22a006a19e7114a
zxh0 <zhangxiuhong2013@gmail.com>
2015-04-01 22:18:37 +0800

    turn GenericTest into unit test

--- testclasses/src/main/java/java7/reflection/GenericTest.java
+++ testclasses/src/main/java/java7/reflection/GenericTest.java
@@ -1,20 +1,41 @@
 package java7.reflection;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import libs.junit.UnitTestRunner;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class GenericTest {
     
+    private static class GenericClass<String> {
+    
+    }
+    
+    private static final String str = "abc";
     private static final List<String> list = new ArrayList<>();
     
     @Test
-    public void fieldGenericType() {
+    public void typeParameter() {
+        assertEquals(1, GenericClass.class.getTypeParameters().length);
+        assertEquals("String", GenericClass.class.getTypeParameters()[0].toString());
+    }
+    
+    @Test
+    public void fieldGenericType() throws NoSuchFieldException {
+        assertEquals(String.class,
+                GenericTest.class.getDeclaredField("str").getGenericType());
         
+        Field listField = GenericTest.class.getDeclaredField("list");
+        assertEquals("list", listField.getName());
+        assertEquals(List.class, listField.getType());
+        assertEquals(26, listField.getModifiers());
+//        System.out.println(f.getGenericType());
     }
     
     public static void main(String[] args) throws Exception {
-        System.out.println(GenericTest.class.getDeclaredField("list").getGenericType());
+        UnitTestRunner.run(GenericTest.class);
     }
     
 }

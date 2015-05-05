1c476de70d6d2a169ab8ed5ea8239b657b60145d
zxh0 <zhangxiuhong2013@gmail.com>
2015-05-06 07:25:06 +0800

    split cp_utf8_string.go

---
+++ jvmgo/classfile/cp_utf8.go
@@ -0,0 +1,19 @@
+package classfile
+
+/*
+CONSTANT_Utf8_info {
+    u1 tag;
+    u2 length;
+    u1 bytes[length];
+}
+*/
+type ConstantUtf8Info struct {
+	str string
+}
+
+func (self *ConstantUtf8Info) readInfo(reader *ClassReader) {
+	self.str = reader.readString()
+}
+func (self *ConstantUtf8Info) Str() string {
+	return self.str
+}

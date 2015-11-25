8b196ad76911fc38e4b3df4a8efdf7c618c19100
zxh0 <zhangxiuhong2013@gmail.com>
2015-11-25 12:14:28 +0800

    rename instruction files
    still broken!!!

---
+++ jvmgo/instructions/math/or.go
@@ -0,0 +1,27 @@
+package instructions
+
+import (
+	"github.com/zxh0/jvm.go/jvmgo/rtda"
+)
+
+// Boolean OR int
+type ior struct{ NoOperandsInstruction }
+
+func (self *ior) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	result := v1 | v2
+	stack.PushInt(result)
+}
+
+// Boolean OR long
+type lor struct{ NoOperandsInstruction }
+
+func (self *lor) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+	result := v1 | v2
+	stack.PushLong(result)
+}
--- jvmgo/instructions/math/xor.go
+++ jvmgo/instructions/math/xor.go
@@ -4,24 +4,24 @@ import (
 	"github.com/zxh0/jvm.go/jvmgo/rtda"
 )
 
-// Boolean OR int
-type ior struct{ NoOperandsInstruction }
+// Boolean XOR int
+type ixor struct{ NoOperandsInstruction }
 
-func (self *ior) Execute(frame *rtda.Frame) {
+func (self *ixor) Execute(frame *rtda.Frame) {
 	stack := frame.OperandStack()
-	v2 := stack.PopInt()
 	v1 := stack.PopInt()
-	result := v1 | v2
+	v2 := stack.PopInt()
+	result := v1 ^ v2
 	stack.PushInt(result)
 }
 
-// Boolean OR long
-type lor struct{ NoOperandsInstruction }
+// Boolean XOR long
+type lxor struct{ NoOperandsInstruction }
 
-func (self *lor) Execute(frame *rtda.Frame) {
+func (self *lxor) Execute(frame *rtda.Frame) {
 	stack := frame.OperandStack()
-	v2 := stack.PopLong()
 	v1 := stack.PopLong()
-	result := v1 | v2
+	v2 := stack.PopLong()
+	result := v1 ^ v2
 	stack.PushLong(result)
 }

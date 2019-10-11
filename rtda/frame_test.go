3d6036e48fb4f72d5d6c79fd1f2896662d1caaa0
zxh0 <zhangxiuhong2013@gmail.com>
2019-10-11 10:21:22 +0800

    Rename files

--- rtda/frame_test.go
+++ rtda/frame_test.go
@@ -24,3 +24,24 @@ func TestLocalVars(t *testing.T) {
 	require.Equal(t, 2.71828, lv.GetDoubleVar(3))
 	require.Equal(t, obj, lv.GetRefVar(4))
 }
+
+func TestOperandStack(t *testing.T) {
+	obj := &heap.Object{}
+
+	stack := newOperandStack(16)
+	require.True(t, stack.IsStackEmpty())
+
+	stack.PushInt(123)
+	stack.PushLong(456)
+	stack.PushFloat(3.14)
+	stack.PushDouble(2.71828)
+	stack.PushRef(obj)
+
+	require.False(t, stack.IsStackEmpty())
+	require.Equal(t, obj, stack.PopRef())
+	require.Equal(t, 2.71828, stack.PopDouble())
+	require.Equal(t, float32(3.14), stack.PopFloat())
+	require.Equal(t, int64(456), stack.PopLong())
+	require.Equal(t, int32(123), stack.PopInt())
+	require.True(t, stack.IsStackEmpty())
+}
